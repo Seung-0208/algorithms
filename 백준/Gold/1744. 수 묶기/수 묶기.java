c85aa91ac3f1ec56f2a54bc9f8f2b3b1f482bb88
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2025-01-13 23:47:25 +0900

    [Gold IV] Title: 수 묶기, Time: 112 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Gold/1744. 수 묶기/수 묶기.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //수열의 모든 수는 단 한번만 묶거나, 아니면 묶지 않아야한다
+        //주어진 수는 음수일 수 있다
+        //음수끼리 곱했을 때 양수가 된다는 성질을 고려하지 못함
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        ArrayList<Integer> ones = new ArrayList<Integer>();
+        ArrayList<Integer> zeros = new ArrayList<Integer>();
+        PriorityQueue<Integer> pos = new PriorityQueue<Integer>(Collections.reverseOrder());
+        PriorityQueue<Integer> neg = new PriorityQueue<Integer>();
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int temp = Integer.parseInt(st.nextToken());
+            if(temp == 0) zeros.add(temp);
+            else if(temp == 1) ones.add(temp);
+            else if(temp < 0) neg.add(temp);
+            else pos.add(temp);
+        }
+
+        long ans = 0;
+        while(pos.size() > 1) {
+            int a = pos.poll(), b = pos.poll();
+            ans += ((long) a *b);
+        }
+        if(pos.size()==1) ans += pos.poll();
+
+        while(neg.size() > 1) {
+            int a = neg.poll(), b = neg.poll();
+            ans += ((long) a*b);
+        }
+        if(neg.size()==1 && zeros.isEmpty()) ans += neg.poll();
+        
+        for(int i=0; i<ones.size(); i++) {
+            ans += 1;
+        }
+
+        System.out.println(ans);
+    }//main
+}
