2b672dc2c7f38f3a5290d950838422f902b58e17
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2025-01-16 14:32:36 +0900

    [Gold V] Title: 거의 소수, Time: 488 ms, Memory: 96172 KB -BaekjoonHub

---
+++ 백준/Gold/1456. 거의 소수/거의 소수.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long[] nums = new long[10000001];
+
+        for(int i=2; i<=10000000; i++) {
+            nums[i] = 1;
+        }
+        //소수 구하기
+        for(int i=2; i<Math.sqrt(nums.length); i++) {
+            if(nums[i] == 1) {
+                int temp = 2;
+                int j = temp * i;
+                while(j<nums.length) {
+                    nums[j] = 0;
+                    temp++;
+                    j = temp*i;
+                }
+            }
+        }
+        //거의 소수 구하기
+        int cnt = 0;
+        for(int i=2; i<nums.length; i++) {
+            if(nums[i]==1) { //i번째 값이 1이면 소수
+                long temp = i;
+                while((double)i <= (double)B/(double)temp) {
+                    if((double)i >= (double)A/(double)temp) cnt++;
+                    temp = temp*i;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
