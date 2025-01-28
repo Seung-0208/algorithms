a8fc4b44e1cc25325e9c6608776041f7d91f25e0
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2025-01-28 19:33:22 +0900

    Update 수들의 합 5.java

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -10,6 +10,7 @@ public class Main{
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(st.nextToken());
 
+        //cnt를 1부터 시작하는 것은, 연속된 자연수의 합으로 나타낼 때 N 자신의 수도 포함되기 때문
         int start = 1, end = 1, sum = 1, cnt = 1;
         while(end != N) {
             if(sum == N) {
