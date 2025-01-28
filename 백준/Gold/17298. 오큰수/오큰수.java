9a4da89a32e7c4fc8433e9b60c4c8e577a944901
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2025-01-28 21:43:40 +0900

    Update 오큰수.java

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,31 +1,38 @@
 import java.io.*;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        Stack<Integer> stack = new Stack<>();
-        int N = Integer.parseInt(st.nextToken()); //수열의 개수
+
+        int N = Integer.parseInt(st.nextToken());
         int[] nums = new int[N];
         int[] ans = new int[N];
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++) nums[i] = Integer.parseInt(st.nextToken());
-        int idx = 0;
-        while(idx <= N-1) {
-            while(!stack.empty() && nums[stack.peek()] < nums[idx]) {
-                ans[stack.pop()] = nums[idx];
+
+        //스택에는 인덱스 값을 기준으로 넣어줘야 한다
+        //인덱스 값을 기준으로 하지 않으면 값을 순서대로 출력할 수 없음
+        Stack<Integer> stack = new Stack<>();
+        int topIdx = 0;
+        while(topIdx <= N-1){
+            while(!stack.isEmpty() && nums[stack.peek()] < nums[topIdx]) {
+                int tmpIdx = stack.pop();
+                ans[tmpIdx] = nums[topIdx];
             }
-            stack.push(idx);
-            idx++;
+            stack.push(topIdx);
+            topIdx++;
         }
-        while(!stack.empty()) {
+
+        //스택에 남아있는 값들은 오큰수가 없다는 뜻이므로 -1로 채워줌
+        while(!stack.isEmpty()) {
             ans[stack.pop()] = -1;
         }
-        for(int i=0; i<N; i++) bw.write(ans[i] + " ");
+
+        for(int i=0; i<N; i++) bw.write(String.valueOf(ans[i])+" ");
         bw.flush();
     }
+
 }
