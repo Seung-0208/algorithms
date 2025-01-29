7b7c464407ff1be7b97fe08793b970384609b520
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2025-01-29 22:14:18 +0900

    [Gold V] Title: 거의 소수, Time: 372 ms, Memory: 56444 KB -BaekjoonHub

--- 백준/Gold/1456. 거의 소수/거의 소수.java
+++ 백준/Gold/1456. 거의 소수/거의 소수.java
@@ -1,40 +1,44 @@
+import java.io.*;
 import java.util.*;
+import java.util.stream.IntStream;
 
 public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        long A = sc.nextLong();
-        long B = sc.nextLong();
-        long[] nums = new long[10000001];
 
-        for(int i=2; i<=10000000; i++) {
-            nums[i] = 1;
-        }
-        //소수 구하기
-        for(int i=2; i<Math.sqrt(nums.length); i++) {
-            if(nums[i] == 1) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+
+        //소수 판정
+        int[] isPrime = new int[10000001];
+        for(int i=2; i<=10000000; i++) isPrime[i] = i;
+        for(int i=2; i<Math.sqrt(10000000); i++) {
+            if(isPrime[i] != 0) {
                 int temp = 2;
-                int j = temp * i;
-                while(j<nums.length) {
-                    nums[j] = 0;
+                int j = i*temp;
+                while(j<=10000000) {
+                    isPrime[j] = 0;
                     temp++;
-                    j = temp*i;
+                    j = i*temp;
                 }
             }
         }
-        //거의 소수 구하기
         int cnt = 0;
-        for(int i=2; i<nums.length; i++) {
-            if(nums[i]==1) { //i번째 값이 1이면 소수
-                long temp = i;
-                //이때 N^k와 B값이 아닌 N과 B/N^(k-1)을 비교 -> 이항정리
-                while((double)i <= (double)B/(double)temp) {
-                    if((double)i >= (double)A/(double)temp) cnt++;
-                    temp = temp*i;
+        for(int i=2; i<=10000000; i++) {
+            if(isPrime[i] != 0) {
+                int temp = 2;
+                long j = (long) Math.pow(i, temp);
+                while(j<=B) {
+                    if(j>=A) cnt++;
+                    temp++;
+                    j = (long) Math.pow(i, temp);
                 }
             }
         }
-
-        System.out.println(cnt);
+        bw.write(String.valueOf(cnt));
+        bw.flush();
     }
 }
