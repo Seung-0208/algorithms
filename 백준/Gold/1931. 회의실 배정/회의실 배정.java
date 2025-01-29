bb24ece8da63b47988e2b8194073f9db3ed87693
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2025-01-29 21:51:55 +0900

    [Gold V] Title: 회의실 배정, Time: 448 ms, Memory: 42024 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -1,49 +1,47 @@
+import java.io.*;
 import java.util.*;
+import java.util.stream.IntStream;
 
-public class Main {
-    public static void main(String[] args) {
-        //회의가 끝나는 시간을 기준으로 그리디 알고리즘 적용
-        //회의가 끝나자마자 다른 회의를 시작할 수 있음 -> end와 start가 같아도 됨 
-        // => 시작하자마자 끝나는 회의도 있음을 고려해야 함.
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        PriorityQueue<Meeting> q = new PriorityQueue<>(new Comparator<Meeting>(){
+public class Main{
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        PriorityQueue<Meeting> q = new PriorityQueue<>(new Comparator<Meeting>() {
             @Override
             public int compare(Meeting o1, Meeting o2) {
-                //(1,2), (2,2)가 있을 때 우선순위 큐에 (2,2)가 먼저 나오면 (1,2)는 반영할 수 없음
-                //(1,2)가 먼저 나오면 (1,2), (2,2) 모두 반영 가능하므로 가능한 회의 수가 늘어남
-                //따라서 end 시간이 같다면 start 시간을 기준으로 오름차순해줘야 함.
-                if(o1.end == o2.end) {
-                    return o1.start - o2.start;
-                }
-                return o1.end - o2.end;
+                int gap = o1.end - o2.end;
+                if(gap==0) return o1.start - o2.start;
+                return gap;
             }
         });
 
         for(int i=0; i<N; i++) {
-            q.add(new Meeting(sc.nextInt(), sc.nextInt()));
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            q.add(new Meeting(start, end));
         }
 
-        int cnt = 1;
-        Meeting cur = q.poll();
+        int cnt = 0;
         while(!q.isEmpty()) {
-            Meeting temp = q.poll();
-            assert cur != null;
-            if(cur.end > temp.start) continue;
-            else {
-                cur = temp;
-                cnt++;
+            cnt++;
+            Meeting cur = q.poll();
+            while(!q.isEmpty() && q.peek().start < cur.end) {
+                q.poll();
             }
         }
         System.out.println(cnt);
-    }//main
+    }
     static class Meeting{
         int start;
         int end;
-
         public Meeting(int start, int end) {
-            this.start = start;
-            this.end = end;
+            this.start =start;
+            this.end= end;
         }
     }
 }
