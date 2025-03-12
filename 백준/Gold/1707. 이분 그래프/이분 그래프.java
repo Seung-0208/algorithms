fc41d2a8bf419eedf5f79d27daa84db4a2fd2c72
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2025-03-12 20:23:04 +0900

    [Gold IV] Title: 이분 그래프, Time: 888 ms, Memory: 269880 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -12,46 +12,39 @@ public class Main{
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int K = Integer.parseInt(st.nextToken());
-        //K번의 케이스가 주어짐.
         for(int i=0; i<K; i++) {
-            isBipartiteGraph = true;
-            //한 케이스 당 정점의 개수와 간선의 개수가 주어짐
             st = new StringTokenizer(br.readLine());
             int V = Integer.parseInt(st.nextToken());
             int E = Integer.parseInt(st.nextToken());
-            //정점의 개수만큼 값을 업데이트
+            //초기화
             visited = new boolean[V+1];
             check = new int[V+1];
             graph = new ArrayList[V+1];
-            for(int j = 1; j<=V; j++) {
-                graph[j] = new ArrayList<>();
-            }
-            //간선의 개수만큼 값을 받아야 함.
+            isBipartiteGraph = true;
+            for(int j=1; j<=V; j++) graph[j] = new ArrayList<>();
+            //간선에 대한 정보 저장
             for(int j=0; j<E; j++) {
                 st = new StringTokenizer(br.readLine());
                 int u = Integer.parseInt(st.nextToken());
                 int v = Integer.parseInt(st.nextToken());
-
                 graph[u].add(v);
                 graph[v].add(u);
             }
-            for(int k=1; k<=V; k++) {
-                DFS(k);
-                if(!isBipartiteGraph) {
-                    break;
-                }
+            //탐색 시작
+            for(int j=1; j<=V; j++) {
+                DFS(j);
+                if(!isBipartiteGraph) break;
             }
             if(isBipartiteGraph) bw.write("YES\n");
             else bw.write("NO\n");
         }
         bw.flush();
     }
-
     static void DFS(int node) {
         visited[node] = true;
         for(int n: graph[node]) {
             if(!visited[n]) {
-                check[n] = (check[node]+1) % 2; //집합 나누기
+                check[n] = (check[node]+1)%2;
                 DFS(n);
             }
             else if(check[n] == check[node]) {
