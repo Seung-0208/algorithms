3dce3ad78220661b47ae5664566f522c954492a1
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2025-04-10 19:47:14 +0900

    [Platinum IV] Title: K번째 최단경로 찾기, Time: 912 ms, Memory: 100224 KB -BaekjoonHub

--- 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.java
+++ 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.java
@@ -8,60 +8,63 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); //도시의 개수
-        int M = Integer.parseInt(st.nextToken()); //도로의 수
-        int k = Integer.parseInt(st.nextToken()); //단계
-        ArrayList<Edge>[] graph = new ArrayList[N+1]; //그래프
-        for(int i=1; i<=N; i++) graph[i] = new ArrayList<>();
-        PriorityQueue<Integer>[] distances = new PriorityQueue[N+1];
+
+        int n = Integer.parseInt(st.nextToken()); //도시의 개수
+        int m = Integer.parseInt(st.nextToken()); //도로의 개수
+        int k = Integer.parseInt(st.nextToken());
+
+        //그래프 정보
+        ArrayList<Edge>[] graph = new ArrayList[n+1];
+        for(int i=1; i<=n; i++) graph[i] = new ArrayList<>();
+        for(int i=0; i<m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            graph[a].add(new Edge(b, c));
+        }
+
+        //거리
+        PriorityQueue<Integer>[] distance = new PriorityQueue[n+1];
         Comparator<Integer> cp = new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
                 return o2-o1;
             }
         };
-        for(int i=1; i<=N; i++) distances[i] = new PriorityQueue<>(k, cp);
-
-        for(int i=0; i<M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            graph[a].add(new Edge(b, w));
-        }
+        for(int i=1; i<=n; i++) distance[i] = new PriorityQueue<>(k, cp);
 
         PriorityQueue<Edge> queue = new PriorityQueue<>();
         queue.add(new Edge(1, 0));
-        distances[1].add(0);
+        distance[1].add(0);
+
         while(!queue.isEmpty()) {
             Edge now = queue.poll();
             for(Edge next : graph[now.node]) {
-                if(distances[next.node].size()<k){
-                    distances[next.node].add(now.weight + next.weight);
-                    queue.add(new Edge(next.node, now.weight + next.weight));
-                } else if(distances[next.node].peek() > now.weight+next.weight) {
-                    distances[next.node].poll();
-                    distances[next.node].add( now.weight+next.weight);
+                if(distance[next.node].size()<k) {
+                    distance[next.node].add(now.weight + next.weight);
+                    queue.add(new Edge(next.node, now.weight+next.weight));
+                } else if(distance[next.node].peek() > now.weight + next.weight) {
+                    distance[next.node].poll();
+                    distance[next.node].add(now.weight + next.weight);
                     queue.add(new Edge(next.node, now.weight+next.weight));
                 }
             }
         }
 
-        for(int i=1; i<=N; i++) {
-            if(distances[i].size() < k) {
-                bw.write(String.valueOf(-1)+"\n");
+        for(int i=1; i<=n; i++) {
+            if(distance[i].size() < k) {
+                bw.write("-1\n");
             } else {
-                bw.write(String.valueOf(distances[i].peek())+"\n");
+                bw.write(String.valueOf(distance[i].peek())+"\n");
             }
         }
 
         bw.flush();
     }
 
-
-    public static class Edge implements Comparable<Edge>{
+    static class Edge implements Comparable<Edge> {
         int node, weight;
-
         public Edge(int node, int weight) {
             this.node = node;
             this.weight = weight;
@@ -72,5 +75,4 @@ public class Main{
             return this.weight - o.weight;
         }
     }
-
 }
