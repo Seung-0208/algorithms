1df8c1f0c41095020d2b5f8f1d0f7be0c38de885
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2025-05-16 18:25:44 +0900

    [Gold III] Title: LCA, Time: 1376 ms, Memory: 42464 KB -BaekjoonHub

--- 백준/Gold/11437. LCA/LCA.java
+++ 백준/Gold/11437. LCA/LCA.java
@@ -3,35 +3,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static ArrayList<Integer>[] tree;
-    static int[] parent;
-    static int[] depth;
-    static boolean[] isVisited;
+    static ArrayList<Integer>[] graph; //연결정보 저장
+    static boolean[] isVisited; //트리 탐색에서 사용할 방문 여부 값
+    static int[] depth; //각 노드가 위치한 height 값
+    static int[] parent; //각 노드의 부모 노드 정보
     public static void main(String[] args) throws IOException{
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken()); //노드의 개수
-        tree = new ArrayList[N+1];
-        for(int i=1; i<=N; i++) tree[i] = new ArrayList<>();
+        int N = Integer.parseInt(st.nextToken());
+        graph = new ArrayList[N+1];
+        for(int i=1; i<=N; i++) graph[i] = new ArrayList<>();
         isVisited = new boolean[N+1];
-        parent = new int[N+1];
         depth = new int[N+1];
+        parent = new int[N+1];
 
         for(int i=0; i<N-1; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
-            tree[a].add(b);
-            tree[b].add(a);
+            graph[a].add(b);
+            graph[b].add(a);
         }
 
-        BFS(1);
+        BFS(1); //루트 노드부터 탐색 시작
 
         st = new StringTokenizer(br.readLine());
         int M = Integer.parseInt(st.nextToken());
+
         for(int i=0; i<M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
@@ -39,20 +38,19 @@ public class Main {
             int LCA = getLCA(a, b);
             bw.write(LCA+"\n");
         }
-
         bw.flush();
         bw.close();
         br.close();
-
     }
 
     static int getLCA(int a, int b) {
-        //깊이가 더 깊은 노드의 값을 a로 설정
+        //a가 b보다 깊이가 깊다고 가정
         if(depth[a] < depth[b]) {
             int temp = a;
             a = b;
             b = temp;
         }
+        //두 노드의 깊이 맞추기
         while(depth[a] != depth[b]) {
             a = parent[a];
         }
@@ -60,32 +58,37 @@ public class Main {
             a = parent[a];
             b = parent[b];
         }
+
         return a;
     }
 
+    //그래프를 탐색하며 트리 정보 저장
     static void BFS(int node) {
-        Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> q = new LinkedList();
         q.add(node);
+
         int level = 1;
-        int nowSize = 1; //같은 레벨의 노드의 개수
+        int childCnt = 1;
         int cnt = 0;
+
         while(!q.isEmpty()) {
             int now = q.poll();
+
+            depth[now] = level;
+
             isVisited[now] = true;
-            for(int next : tree[now]) {
+            for(int next : graph[now]) {
                 if(!isVisited[next]) {
                     q.add(next);
                     parent[next] = now;
-                    depth[next] = level;
                 }
             }
             cnt++;
-            if(cnt == nowSize) {
-                cnt = 0;
-                nowSize = q.size();
+            if(childCnt == cnt) {
                 level++;
+                childCnt = q.size();
+                cnt = 0;
             }
         }
-
     }
 }
