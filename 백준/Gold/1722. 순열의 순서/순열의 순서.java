66dd6d5a9544f13910d0e32d56f53e6b9a490158
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2025-05-29 20:59:12 +0900

    [Gold V] Title: 순열의 순서, Time: 116 ms, Memory: 15860 KB -BaekjoonHub

--- 백준/Gold/1722. 순열의 순서/순열의 순서.java
+++ 백준/Gold/1722. 순열의 순서/순열의 순서.java
@@ -8,32 +8,31 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); //숫자의 개수
-        boolean[] isUsed = new boolean[N+1];
-        //경우의 수
+        int N = Integer.parseInt(st.nextToken());
         long[] cases = new long[N+1];
         cases[N] = 1;
         cases[N-1] = 1;
-        //경우의 수 초기화
+        //0 1 2 3
+        //4 3 2 1
         for(int i=N-2; i>=0; i--) {
-            cases[i] = (N-i)*cases[i+1];
+            cases[i] = cases[i+1] * (N-i);
         }
 
+        boolean[] isUsed = new boolean[N+1];
 
         st = new StringTokenizer(br.readLine());
         int p = Integer.parseInt(st.nextToken()); //소문제 번호
 
-        if(p==1) { //수열을 구해야함
+        int[] permutation = new int[N+1];
+        if(p==1) { //순열 구하기
             long k = Long.parseLong(st.nextToken());
-            int[] permutation = new int[N+1]; //순열
             for(int i=1; i<=N; i++) {
                 for(int j=1, cnt=1; j<=N; j++) {
-                    if(isUsed[j])
-                        continue;
-                    if(k <= cases[i] * cnt) {
+                    if(isUsed[j]) continue;
+                    if(k <= cnt * cases[i]) {
                         permutation[i] = j;
                         isUsed[j] = true;
-                        k = k - (cases[i]*(cnt-1));
+                        k -= cases[i] * (cnt-1);
                         break;
                     }
                     cnt++;
@@ -44,24 +43,23 @@ public class Main {
                 bw.write(permutation[i]+" ");
             }
             bw.write("\n");
-        } else { //순서 구하기
-            long k = 1;
-            int[] permutation = new int[N+1];
-            for(int i=1; i<=N; i++){
+        } else if(p==2) { //순서 구하기
+            for(int i=1; i<=N; i++) {
                 permutation[i] = Integer.parseInt(st.nextToken());
             }
-
-            for(int i=1; i<=N; i++) { //주어진 수열의 현재 자릿수
-                isUsed[permutation[i]] = true;
+            long k = 1;
+            for(int i=1; i<=N; i++) {
                 int cnt = 0;
+                isUsed[permutation[i]] = true;
                 for(int j=1; j<=permutation[i]; j++) {
                     if(!isUsed[j]) cnt++;
                 }
-                k += cases[i] * cnt;
+                k += cnt * cases[i];
             }
-
             bw.write(k+"\n");
         }
+
+
 
         bw.flush();
         bw.close();
