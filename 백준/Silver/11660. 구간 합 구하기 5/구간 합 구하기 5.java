4d9f1a50a826ad493543df7fb8064fd2b5bba561
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2025-07-23 21:36:47 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 804 ms, Memory: 125428 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,39 +1,41 @@
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+
+import java.io.*;
+import java.math.BigDecimal;
 import java.util.*;
 
-public class Main{
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
+public class Main {
+
+    public static void main(String[] args) throws IOException{
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int[][] nums = new int[N+1][N+1];
-        int[][] haps = new int[N+1][N+1];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for(int i=1; i<=N; i++) {
-            for(int j=1; j<=N; j++) {
-                nums[i][j] = sc.nextInt();
-            }
-        }
+        int N= Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
+        int[][] D = new int[N+1][N+1];
+        int[][] S = new int[N+1][N+1];
         for(int i=1; i<=N; i++) {
-            for(int j=1; j<=N; j++) {
-                haps[i][j] = haps[i-1][j] + haps[i][j-1] - haps[i-1][j-1] + nums[i][j];
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                D[i][j] = Integer.parseInt(st.nextToken());
+                S[i][j] = S[i-1][j] + S[i][j-1] + D[i][j] - S[i-1][j-1];
             }
         }
 
         for(int i=0; i<M; i++) {
-            int[] xy = new int[4];
-            for(int j=0; j<4; j++) {
-                xy[j] = sc.nextInt();
-            }
-            int x1 = xy[0], y1 = xy[1], x2 = xy[2], y2 = xy[3];
-            int temp = haps[x2][y2] - haps[x1-1][y2] - haps[x2][y1-1] + haps[x1-1][y1-1];
-            bw.write(String.valueOf(temp));
-            bw.newLine();
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int ans = S[x2][y2] - S[x2][y1-1] - S[x1-1][y2] + S[x1-1][y1-1];
+            bw.write(ans+"\n");
         }
+
         bw.flush();
+        bw.close();
+        br.close();
     }
 }
