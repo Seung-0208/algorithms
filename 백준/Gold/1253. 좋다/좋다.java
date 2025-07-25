f9b2564e5a3bf1b22fd44b56898c41608c444846
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2025-07-25 22:29:16 +0900

    [Gold IV] Title: 좋다, Time: 160 ms, Memory: 15128 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,40 +1,35 @@
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.*;
 import java.util.stream.IntStream;
-import java.util.stream.LongStream;
 
 public class Main{
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
         long[] nums = new long[N];
-        for(int i=0; i<N; i++) nums[i] = sc.nextLong();
-
-        Arrays.sort(nums);
-
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) nums[i] = Long.parseLong(st.nextToken());
+        Arrays.sort(nums); //0번
         int cnt = 0;
-        for(int i=0; i<N; i++) {
+        for(int i=0; i<N; i++) { //1번
+            int s_pt = 0, e_pt = N-1;
             long cur = nums[i];
-            int start = 0, end = N-1;
-            while(start < end) {
-                long tmp = nums[start] + nums[end];
-                if(cur > tmp) start++;
-                else if(cur < tmp) end--;
-                else {
-                    if(start != i && end != i) {
+            while(s_pt<e_pt) { //2번
+                long temp = nums[s_pt] + nums[e_pt];
+                if(cur > temp) s_pt++;
+                else if(cur < temp) e_pt--;
+                else { //만약 둘이 같다면
+                    if(s_pt != i && e_pt != i) {
                         cnt++;
                         break;
-                    } 
-                    //start와 end가 모두 i인 경우가 있을 수 있으므로 else if가 아닌 if로 작성
-                    if(start==i) start++;
-                    if(end==i) end--;
+                    }
+                    else if(s_pt==i) s_pt++;
+                    if(e_pt==i) e_pt--;
                 }
             }
         }
-
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         bw.write(String.valueOf(cnt));
         bw.flush();
     }
