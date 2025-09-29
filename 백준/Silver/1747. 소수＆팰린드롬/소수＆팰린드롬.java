664530cdf9e46b394777e92a793adef79ab6144d
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2025-09-29 11:19:32 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 272 ms, Memory: 31248 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
@@ -1,40 +1,51 @@
+import java.io.*;
 import java.util.*;
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] nums = new int[10000001];
-        for(int i=2; i<nums.length; i++) nums[i] = i;
-        //소수 판정
-        for(int i=2; i<Math.sqrt(nums.length); i++) {
-            if(nums[i] != 0) {
-                int temp = 2;
-                int j = i * temp;
-                while(j<=10000000) {
-                    nums[j] = 0;
-                    temp++;
-                    j = i*temp;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(st.nextToken());
+        int RANGE = 10000001;
+        boolean[] isDeleted = new boolean[RANGE];
+        List<Integer> primes = new ArrayList<>();
+        isDeleted[1] = true;
+
+        for(int i=2; i<Math.sqrt(RANGE); i++) {
+            if(!isDeleted[i]) {
+                primes.add(i);
+                for(int j=i*2; j<RANGE; j+=i) {
+                    isDeleted[j] = true;
                 }
             }
         }
 
-        while(true) {
-            if(nums[N] != 0) {
-                String numberString = Integer.toString(N);
-                char[] temp = numberString.toCharArray();
-                int start = 0, end = temp.length-1;
-                boolean isSame = true;
-                while(start <= end) {
-                    if(temp[start] != temp[end]) {
-                        isSame = false;
-                        break;
-                    }
-                    start++; end--;
-                }
-                if(isSame) break;
+        int ans = 0;
+        for(int i=N; i<RANGE; i++) {
+            if(!isDeleted[i] && isPalindromic(i)) {
+                ans = i;
+                break;
             }
-            N++;
         }
-        System.out.println(N);
+
+        bw.write(ans+"\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static boolean isPalindromic(int num) {
+        String temp = String.valueOf(num);
+
+        int s = 0, e = temp.length()-1;
+
+        while(s<=e) {
+            if(temp.charAt(s) != temp.charAt(e)) return false;
+            s++; e--;
+        }
+
+        return true;
     }
 }
