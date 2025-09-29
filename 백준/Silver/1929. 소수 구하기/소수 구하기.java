6f9e19cedbd7f5d5a1f12b1ca8caf89cbcbcb915
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2025-09-29 10:28:05 +0900

    [Silver III] Title: 소수 구하기, Time: 276 ms, Memory: 23136 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,34 +1,40 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.IntStream;
 
-public class Main{
-
-    public static void main(String[] args) throws IOException {
+public class Main {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out));
+
         int M = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
 
-        int[] isPrime = new int[N+1];
-        for(int i=2; i<N+1; i++) isPrime[i] = i;
-        for(int i=2; i<=Math.sqrt(N); i++) {
-            if(isPrime[i] != 0) {
-                int temp = 2;
-                int j = i*temp;
-                while(j<=N) {
-                    isPrime[j] = 0;
-                    temp++;
-                    j = i*temp;
+        boolean[] isDeleted = new boolean[N+1];
+        isDeleted[1] = true;
+
+        for(int i=4; i<=N; i+=2) {
+            isDeleted[i] = true;
+        }
+
+        for(int i=6; i<=N; i+=3) {
+            isDeleted[i] = true;
+        }
+
+        for(int i=5; i<=N; i++) {
+            if(!isDeleted[i]) {
+                for(int j=i*2; j<=N; j+=i) {
+                    isDeleted[j] = true;
                 }
             }
         }
 
         for(int i=M; i<=N; i++) {
-            if(isPrime[i] != 0) bw.write(String.valueOf(i)+"\n");
+            if(!isDeleted[i]) bw.write(i+"\n");
         }
+
         bw.flush();
+        bw.close();
+        br.close();
     }
 }
