e72bf4a4cdc0fae3d0b8504fbd469c8b5f5eda1d
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2025-09-30 11:10:56 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 116 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
@@ -1,33 +1,31 @@
 import java.io.*;
 import java.util.*;
+import java.util.stream.IntStream;
 
 public class Main{
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        //GCD = 최대공약수
-
-        //입력값이자, 따지고 보면 배열의 역할을 함.
-        //소인수의 배수를 n에서 지워주기 때문
+        //n과 k의 최대공약수가 1이다 = 최대공약수가 존재하지 않는다 = n과 서로소인 자연수의 개수
         long n = Long.parseLong(st.nextToken());
 
-        long ret = n;
-        for(long p = 2; p<=Math.sqrt(n); p++) {
-            if(n%p==0) { //p가 소인수 (약수 중 소수인 숫자)
-                ret = ret - ret/p;
-                while(n%p == 0){ //p(소인수)의 배수에 해당하는 값 지우기
-                    n /= p;
+        long result = n;
+
+        for(long i=2; i <= Math.sqrt(n); i++) {
+            if(n % i == 0) { //i는 result의 약수
+                result = result - result/i;
+                while(n%i == 0) {
+                    n /= i;
                 }
             }
         }
-
-        //n이 1보다 크면 n이 마지막 소수라는 의미
-        if(n > 1) ret = ret-ret/n;
-        bw.write(String.valueOf(ret));
+        if (n>1) {
+            result = result - result/n;
+        }
+        bw.write(String.valueOf(result));
         bw.flush();
-        bw.close();
     }
 }
