8303e76f9e358e047f7135931dc219483c6f0809
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2025-09-30 10:16:01 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 212 ms, Memory: 18956 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
@@ -1,25 +1,35 @@
+import java.io.*;
 import java.util.*;
 
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        long min = sc.nextLong();
-        long max = sc.nextLong();
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        boolean[] isSquare = new boolean[(int)(max-min+1)];
-        for(long i=2; i*i<=max; i++) {
-            long square = i*i;
-            long startIdx = min/square;
-            if(min%square != 0) startIdx++;
-            for(long j=startIdx; j*square <= max; j++) {
-                isSquare[(int)(j*square-min)] = true;
+        long min = Long.parseLong(st.nextToken());
+        long max = Long.parseLong(st.nextToken());
+
+        boolean[] isDeleted = new boolean[(int)(max-min+1)];
+
+        for(int i=2; i<1000001; i++) {
+            long square = (long)i*i;
+            long temp = min/square;
+            long start = square*temp;
+            for(long j = start; j<=max; j+=square) {
+                if(j >= min) {
+                    isDeleted[(int)(j-min)] = true;
+                }
             }
         }
-
         int cnt = 0;
-        for(boolean isTrue : isSquare) {
-            if(!isTrue) cnt++;
+        for(boolean t : isDeleted) {
+            if(!t) cnt++;
         }
-        System.out.println(cnt);
+
+        bw.write(cnt+"\n");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
