2f88a86a46e490fbf76217593d93919bc212e95b
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2025-10-09 23:49:09 +0900

    [Gold II] Title: 역원(Inverse) 구하기, Time: 104 ms, Memory: 14436 KB -BaekjoonHub

--- 백준/Gold/14565. 역원（Inverse） 구하기/역원（Inverse） 구하기.java
+++ 백준/Gold/14565. 역원（Inverse） 구하기/역원（Inverse） 구하기.java
@@ -1,44 +1,45 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.IntStream;
 
-public class Main{
-    public static void main(String[] args) throws IOException {
+public class Main {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         long N = Long.parseLong(st.nextToken());
         long A = Long.parseLong(st.nextToken());
 
-        bw.write(String.valueOf(N-A)+" ");
+        /*
+        * 곱셈역 구할 때
+        * Ax + Ny = 1 을 만족하는 x값을 구해야 함
+        * */
 
-        long r1 = A, r2 = N;
-        long x1 = 1, x2 = 0;
-
-        long multi;
-
+        long r1 = A;
+        long r2 = N;
+        long x1 = 1;
+        long x2 = 0;
+        
         while(r2 > 0) {
-            long quotient = r1 / r2;
-
+            long q = r1/r2;
             long temp = r1;
             r1 = r2;
-            r2 = temp - quotient * r2;
-
+            r2 = temp - r2*q;
+            
             temp = x1;
             x1 = x2;
-            x2 = temp - quotient * x2;
+            x2 = temp - x2*q;
         }
-
+        
+        long multi = x1;
         if(r1 == 1) {
-            if(x1 <= 0) x1 += N;
-            multi = x1;
-        } else {
-            multi = -1;
-        }
+            if(x1 <= 0) multi += N;
+        } else multi = -1;
+        
+        bw.write(String.format("%d %d\n", N-A, multi));
 
-        bw.write(String.valueOf(multi));
         bw.flush();
+        br.close();
         bw.close();
     }
-
 }
