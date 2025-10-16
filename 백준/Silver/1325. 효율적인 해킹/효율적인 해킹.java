5e28d597f0a799a47c600f7024d8962c283877a6
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2025-10-17 00:16:43 +0900

    [Silver I] Title: 효율적인 해킹, Time: 9956 ms, Memory: 305572 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -1,61 +1,57 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.IntStream;
 
-public class Main{
-    static ArrayList<Integer>[] graph;
-    static int[] record;
-    static boolean[] visited;
-    static int N;
-    public static void main(String[] args) throws IOException {
+public class Main {
+    static ArrayList<Integer>[] rev;
+    static int[] seen, cnt;
+    static int N, M;
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); //N개의 컴퓨터
-        int M = Integer.parseInt(st.nextToken()); //M개의 경우의 수
-        graph = new ArrayList[N+1];
-        record = new int[N+1];
-        visited = new boolean[N+1];
-        for(int i=1; i<=N; i++) {
-            graph[i] = new ArrayList<>();
-        }
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        for(int i=0; i<M; i++) {
+        rev = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) rev[i] = new ArrayList<>();
+        seen = new int[N + 1];
+        cnt = new int[N + 1];
+
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            graph[A].add(B);
-        }
-
-        for(int i=1; i<=N; i++) {
-            BFS(i);
+            int a = Integer.parseInt(st.nextToken()); // a가 b를 신뢰
+            int b = Integer.parseInt(st.nextToken());
+            rev[b].add(a); // 해킹 전파: b -> a
         }
 
         int max = 0;
-        for(int i=1; i<=N; i++) {
-            if(record[i] > max) max = record[i];
+        for (int s = 1; s <= N; s++) {
+            int c = bfs(s);
+            cnt[s] = c;
+            if (c > max) max = c;
         }
 
-        for(int i=1; i<=N; i++) {
-            if(record[i]==max) bw.write(String.valueOf(i) + " ");
-        }
-        bw.flush();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) if (cnt[i] == max) sb.append(i).append(' ');
+        System.out.print(sb.toString());
     }
-    static void BFS(int n) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(n);
-        visited = new boolean[N+1];
-        visited[n] = true;
-        while(!queue.isEmpty()) {
-            int cur = queue.poll();
-            for(int node : graph[cur]) {
-                if(!visited[node]) {
-                    visited[node] = true;
-                    queue.add(node);
-                    record[node] += 1;
+
+    static int bfs(int start) {
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        q.add(start);
+        seen[start] = start;
+        int c = 0;
+
+        while (!q.isEmpty()) {
+            int u = q.poll();
+            for (int v : rev[u]) {
+                if (seen[v] != start) {
+                    seen[v] = start;
+                    q.add(v);
+                    c++;
                 }
             }
         }
-
+        return c;
     }
 }
