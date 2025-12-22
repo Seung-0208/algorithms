ab6e0a1a261ccb0e19c4b0a71b72f60769c06424
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2025-12-22 14:54:20 +0900

    [Gold III] Title: 줄 세우기, Time: 460 ms, Memory: 51660 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,47 +1,52 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
+    static ArrayList<Integer>[] graph;
+    static int[] edgeCnt;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); //학생의 수
-        int M = Integer.parseInt(st.nextToken()); //키를 비교한 횟수
 
-        Map<Integer,Integer> temp = new HashMap<>(); //진입차수 정보
-        for (int i=1; i<=N; i++) temp.put(i, 0);
-        ArrayList<Integer>[] graph = new ArrayList[N+1]; //연결정보
-        for(int i=1; i<N+1; i++) graph[i] = new ArrayList<>();
+        int N = Integer.parseInt(st.nextToken()); //학생 수
+        int M = Integer.parseInt(st.nextToken()); //비교 횟수
+
+        graph = new ArrayList[N+1];
+        edgeCnt = new int[N+1];
+        for(int i=1; i<=N; i++) graph[i] = new ArrayList<>();
 
         for(int i=0; i<M; i++) {
             st = new StringTokenizer(br.readLine());
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
-            temp.put(B, temp.get(B)+1);
-            graph[A].add(B);
-        }
 
-        Queue<Integer> queue = new LinkedList<>();
+            graph[A].add(B);
+            edgeCnt[B]++;
+        }
+        Queue<Integer> q = new LinkedList<Integer>();
+
         for(int i=1; i<=N; i++) {
-            if(temp.get(i) == 0) {
-                queue.add(i);
-                temp.remove(i);
+            if(edgeCnt[i] == 0) {
+                q.add(i);
             }
         }
 
-        while(!queue.isEmpty()) {
-            int curr = queue.poll();
-            bw.write(String.valueOf(curr)+" ");
-            for(Integer neigh : graph[curr]) {
-                temp.put(neigh, temp.get(neigh)-1);
-                if(temp.get(neigh) == 0) {
-                    queue.add(neigh);
-                    temp.remove(neigh);
+
+        while(!q.isEmpty()) {
+            int curr = q.poll();
+            bw.write(curr+" ");
+            for(int n : graph[curr]) {
+                if(edgeCnt[n] > 0) {
+                    edgeCnt[n]--;
+                    if(edgeCnt[n] == 0) q.add(n);
                 }
             }
         }
-        bw.newLine();
+        bw.write("\n");
         bw.flush();
+        bw.close();
+        br.close();
+
     }
 }
