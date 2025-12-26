593cdf715db7e6bb2948108df38979c5e6ff0143
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2025-12-26 17:00:30 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 476 ms, Memory: 48888 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,70 +1,64 @@
-import org.w3c.dom.Node;
-
 import java.io.*;
 import java.util.*;
 
-public class Main{
-    static int[] graph;
+public class Main {
+    static int[] parent;
+    static int sum;
+    static int edgeCnt;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        int V = Integer.parseInt(st.nextToken()); //정점의 개수
-        graph = new int[V+1];
-        for(int i=1; i<=V; i++) graph[i] = i;
+        int V = Integer.parseInt(st.nextToken()); //정점
+        PriorityQueue<Edge> q = new PriorityQueue<>();
+        parent = new int[V+1];
+        for(int i=1; i<=V; i++) parent[i] = i;
+        int E = Integer.parseInt(st.nextToken()); //간선
 
-        int E = Integer.parseInt(st.nextToken()); //간선의 개수
-
-        PriorityQueue<Edge> edges = new PriorityQueue<>();
         for(int i=0; i<E; i++) {
             st = new StringTokenizer(br.readLine());
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
             int C = Integer.parseInt(st.nextToken());
-            edges.add(new Edge(A, B, C));
+            q.add(new Edge(A, B, C));
         }
 
-        int edgeN = 0;
-        long sum = 0;
-        while(!edges.isEmpty()) {
-            Edge edge = edges.poll();
-            int A = edge.node1;
-            int B = edge.node2;
-            int C = edge.weight;
-            graph[A] = find(A);
-            graph[B] = find(B);
-            if(graph[A] != graph[B]) {
-                graph[graph[A]] = graph[B];
-                edgeN++;
-                sum+=C;
-            }
-            if(edgeN == V-1) break;
+        while(!q.isEmpty()) {
+            Edge curr = q.poll();
+            merge(curr.a, curr.b, curr.weight);
+            if(edgeCnt == V-1) break;
         }
 
-        bw.write(sum+"\n");
-        bw.flush();
-
-
+        sb.append(sum).append("\n");
+        System.out.println(sb);
     }
 
-    static class Edge implements Comparable<Edge>{
-        int node1, node2, weight;
-        public Edge(int node1, int node2, int weight) {
-            this.node1 = node1;
-            this.node2 = node2;
+    static int find(int node) {
+        if(node == parent[node]) return node;
+        return find(parent[node]);
+    }
+
+    static void merge(int a, int b, int weight) {
+        parent[a] = find(a);
+        parent[b] = find(b);
+        if(parent[a]==parent[b]) return;
+        parent[parent[a]] = parent[b];
+        edgeCnt++;
+        sum+=weight;
+    }
+
+    static class Edge implements Comparable<Edge> {
+        int a; int b; int weight;
+        public Edge(int a, int b, int weight) {
+            this.a = a;
+            this.b = b;
             this.weight = weight;
         }
 
         @Override
-        public int compareTo(Edge o) {
-            return this.weight - o.weight;
+        public int compareTo(Edge o1) {
+            return this.weight - o1.weight;
         }
-
-    }
-
-    static int find(int node){
-        if(graph[node] == node) return node;
-        return find(graph[node]);
     }
 }
