0f8980e97692a2dd7daddfbfad254fca27929756
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2025-12-26 16:12:19 +0900

    [Gold IV] Title: 플로이드, Time: 380 ms, Memory: 44644 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,52 +1,50 @@
-import org.w3c.dom.Node;
-
 import java.io.*;
 import java.util.*;
 
-public class Main{
-    static int MAX_VALUE =10000001;
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(st.nextToken()); //도시의 개수
-        int[][] D = new int[n+1][n+1];
+        int[][] distance = new int[n+1][n+1];
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=n; j++) {
-                if(i!=j) D[i][j] = MAX_VALUE;
-                else D[i][j] = 0;
+                if(i==j) distance[i][j] = 0;
+                else distance[i][j] = Integer.MAX_VALUE;
             }
         }
         st = new StringTokenizer(br.readLine());
-        int m = Integer.parseInt(st.nextToken()); //버스의 개수
+        int m = Integer.parseInt(st.nextToken());
 
+        //초기화
         for(int i=0; i<m; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            if(D[a][b] > c) D[a][b] = c;
+            int a = Integer.parseInt(st.nextToken()); //시작
+            int b = Integer.parseInt(st.nextToken()); //끝
+            int c = Integer.parseInt(st.nextToken()); //비용
+            if(distance[a][b] > c) distance[a][b] = c;
         }
 
         for(int k=1; k<=n; k++) {
             for(int s=1; s<=n; s++) {
                 for(int e=1; e<=n; e++) {
-                    if(D[s][e] > D[s][k]+D[k][e]) {
-                        D[s][e] = D[s][k] + D[k][e];
-
-                    }
+                    if(distance[s][k] != Integer.MAX_VALUE && distance[k][e] != Integer.MAX_VALUE && distance[s][e] > distance[s][k] + distance[k][e])
+                        distance[s][e] = distance[s][k] + distance[k][e];
                 }
             }
         }
 
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=n; j++) {
-                if(D[i][j]==MAX_VALUE) bw.write("0 ");
-                else bw.write(D[i][j]+" ");
+                if(distance[i][j] == Integer.MAX_VALUE) sb.append("0 ");
+                else sb.append(distance[i][j]).append(" ");
             }
-            bw.write("\n");
+            sb.append("\n");
         }
-        bw.flush();
+
+        System.out.println(sb);
+        br.close();
     }
 }
