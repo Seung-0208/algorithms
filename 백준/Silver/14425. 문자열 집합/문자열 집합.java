3563cbb0fe6874193b8fa7e6f344878a84b04ff9
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-01-02 17:38:35 +0900

    [Silver IV] Title: 문자열 집합, Time: 2376 ms, Memory: 873324 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,15 +1,14 @@
-
 import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken()); //문자열의 개수
+        int M = Integer.parseInt(st.nextToken()); //검사할 문자열의 개수
 
         Node root = new Node();
         for(int i=0; i<N; i++) {
@@ -18,34 +17,36 @@ public class Main{
             Node now = root;
             for(int j=0; j<tmp.length(); j++) {
                 char t = tmp.charAt(j);
-                if(now.children[t-'a'] == null) {
-                    now.children[t-'a'] = new Node();
+                if(now.characters[t-'a'] == null) {
+                    now.characters[t-'a'] = new Node();
                 }
-                now = now.children[t-'a'];
-                if(j == tmp.length()-1) now.isLeaf = true;
+                now = now.characters[t-'a'];
+                if(j==tmp.length()-1) {
+                    now.isLeaf = true;
+                }
             }
         }
-        int cnt = 0;
+        int sum = 0;
         for(int i=0; i<M; i++) {
             st = new StringTokenizer(br.readLine());
             String tmp = st.nextToken();
             Node now = root;
             for(int j=0; j<tmp.length(); j++) {
                 char t = tmp.charAt(j);
-                if(now.children[t-'a'] == null) break;
-                now = now.children[t-'a'];
-                if(j==tmp.length()-1 && now.isLeaf) cnt++;
+                now = now.characters[t-'a'];
+                if(now == null) break;
+                if(j==tmp.length()-1 && now.isLeaf) sum++;
             }
         }
 
-        bw.write(cnt+"\n");
-        bw.flush();
+        sb.append(sum);
+        System.out.println(sb);
+        br.close();
 
     }
 
-    static class Node{
-        Node[] children = new Node[26];
+    static class Node {
+        Node[] characters = new Node[26];
         boolean isLeaf;
     }
-
 }
