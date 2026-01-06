dad63ff9e701fc64a268debade441baca7100788
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-01-06 16:35:52 +0900

    [Gold I] Title: 구간 합 구하기, Time: 588 ms, Memory: 105000 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -1,85 +1,64 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
     static long[] tree;
-    public static void main(String[] args) throws IOException{
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(st.nextToken()); //수의 개수
         int M = Integer.parseInt(st.nextToken()); //수의 변경 횟수
-        int K = Integer.parseInt(st.nextToken()); //구간의 합을 구하는 횟수
+        int K = Integer.parseInt(st.nextToken()); //구간의 합 횟수
 
-        int length = N;
-        int treeHeight = 0;
-        while(length > 0) {
-            treeHeight++;
-            length /= 2;
+        int leafStartIdx = 1;
+        while(leafStartIdx < N) {
+            leafStartIdx *= 2;
         }
-        if((int)Math.pow(2, treeHeight-1) == N) treeHeight--;
-        int treeSize = (int)Math.pow(2,treeHeight+1);
-        int treeLeafStart = treeSize/2;
+        int treeSize = leafStartIdx * 2;
         tree = new long[treeSize];
-        for(int i=treeLeafStart; i <= treeLeafStart+N-1; i++) {
+
+        // 트리 초기화
+        for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
-            tree[i] = Long.parseLong(st.nextToken());
+            tree[i+leafStartIdx] = Long.parseLong(st.nextToken());
+        }
+        for(int i=leafStartIdx-1; i>0; i--) {
+            tree[i] = tree[i*2] + tree[i*2+1];
         }
 
-        fillTree(treeLeafStart - 1);
         for(int i=0; i<M+K; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            long c = Long.parseLong(st.nextToken());
             if(a==1) {
-                b = b + treeLeafStart - 1;
-                changeNode(b, c);
+                long c = Long.parseLong(st.nextToken());
+                b += (leafStartIdx-1);
+                tree[b] = c;
+                while(true) {
+                    b /= 2;
+                    if(b==0) break;
+                    tree[b] = tree[b*2] + tree[b*2+1];
+                }
             }
-            else if(a==2) {
-                long result = getPartSum(b, (int) c, treeLeafStart);
-                bw.write(result+"\n");
+            if(a==2) {
+                int c = Integer.parseInt(st.nextToken());
+                int start = b + (leafStartIdx-1);
+                int end = c + (leafStartIdx-1);
+                long total = 0;
+
+                while(start <= end) {
+                    if(start % 2 == 1) total += tree[start];
+                    if(end % 2 == 0) total += tree[end];
+                    start = (start+1)/2;
+                    end = (end-1)/2;
+                }
+
+                sb.append(total).append("\n");
             }
         }
-
-        bw.flush();
+        System.out.println(sb);
         br.close();
-    }
-
-    static void fillTree(int i) {
-        while(i > 0) {
-            tree[i] = tree[2*i] + tree[2*i + 1];
-            i--;
-        }
-    }
-
-    static void changeNode(int idx, long val) {
-        long interval = val - tree[idx];
-        while(idx > 0) {
-            tree[idx] += interval;
-            idx /= 2;
-        }
-    }
-
-    static long getPartSum(int start, int end, int leafNodeStart) {
-        //주어진 인덱스를 세그먼트 트리 인덱스로 변경
-        start = start + leafNodeStart - 1;
-        end = end + leafNodeStart - 1;
-        long ret = 0;
-        while(start <= end) {
-            if(start%2==1) {
-                ret += tree[start];
-                start++;
-            }
-            if(end%2==0) {
-                ret += tree[end];
-                end--;
-            }
-            start/=2;
-            end/=2;
-        }
-        return ret;
     }
 }
