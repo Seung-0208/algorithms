542e53b66b2c8f396d90fdd14494b6c83306abf6
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-01-16 18:00:16 +0900

    [Silver IV] Title: ATM, Time: 108 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,37 +1,28 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] nums = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        LinkedList<Integer> P = new LinkedList<>();
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++) P.add(Integer.parseInt(st.nextToken()));
-
-
-        //삽입 정렬
-        for(int i=1; i<N; i++) {
-            int temp = P.get(i);
-            if(temp>=P.get(i-1)) continue;
-            int j = 0;
-            for(j=0; j<=i-1; j++) {
-                if(temp<=P.get(j)) break;
-            }
-            P.remove(i);
-            P.add(j, temp);
-        }
-
-        int[] fin = new int[N];
         for(int i=0; i<N; i++) {
-            fin[i] = P.get(i);
+            nums[i] = Integer.parseInt(st.nextToken());
         }
+
+        Arrays.sort(nums);
+        int total = 0;
         for(int i=1; i<N; i++) {
-            fin[i] += fin[i-1];
+            nums[i] = nums[i] + nums[i-1];
+            total += nums[i];
         }
-        bw.write(String.valueOf(Arrays.stream(fin).sum()));
-        bw.flush();
+        total+=nums[0];
+
+        sb.append(total).append("\n");
+        System.out.println(sb);
+        br.close();
     }
 }
