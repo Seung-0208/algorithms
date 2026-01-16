086ed713e518bc72e2d9db151f95bbbb7adcfcc7
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-01-16 19:17:10 +0900

    [Gold V] Title: 집합의 표현, Time: 496 ms, Memory: 52708 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,48 +1,48 @@
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] parents;
+    static int[] parent;
     public static void main(String[] args) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        parent = new int[n+1];
+        for(int i=0; i<=n; i++) parent[i] = i;
 
-        int n = Integer.parseInt(st.nextToken()); //0부터 n까지의 집합
-        parents = new int[n+1];
-        for(int i=0; i<=n; i++) parents[i] = i;
-        int m = Integer.parseInt(st.nextToken()); //연산의 개수
-
+        int m = Integer.parseInt(st.nextToken()); //케이스 개수
         for(int i=0; i<m; i++) {
             st = new StringTokenizer(br.readLine());
-            int type = Integer.parseInt(st.nextToken());
+            int operator = Integer.parseInt(st.nextToken());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            parents[a] = find(a);
-            parents[b] = find(b);
-            if(type == 0) { //합집합
-                union(a,b);
-            } else if(type == 1) { //같은집합인지 계산
-                if(parents[a] == parents[b]) {
-                    bw.write("YES\n");
-                } else {
-                    bw.write("NO\n");
-                }
+            if(operator == 0) union(a, b);
+            else {
+                parent[a] = find(a);
+                parent[b] = find(b);
+                if(parent[a] == parent[b]) sb.append("YES\n");
+                else sb.append("NO\n");
             }
         }
-        bw.flush();
-        bw.close();
+
+        System.out.println(sb);
         br.close();
     }
 
-    static int find(int n) {
-        if(parents[n] == n) return n;
-        return find(parents[n]);
+    static int find(int a) {
+        if(a==parent[a]) return a;
+        return find(parent[a]);
     }
-    
+
     static void union(int a, int b) {
-        if(parents[a] != parents[b]) {
-            parents[parents[a]] = parents[b];
-        }
+        parent[a] = find(a);
+        parent[b] = find(b);
+
+        if(parent[a]==parent[b]) return;
+        parent[parent[a]] = parent[b];
     }
+
 }
