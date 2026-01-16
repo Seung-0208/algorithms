d4edfad40ddd110387e4b673a41756685133060d
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-01-16 22:11:44 +0900

    [Gold IV] Title: 오큰수, Time: 912 ms, Memory: 156276 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,22 +1,20 @@
 
 import java.io.*;
-import java.math.BigDecimal;
 import java.util.*;
-import java.util.stream.LongStream;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken()); //수의 개수
         int[] ret = new int[N];
-        Stack<Node> stack = new Stack<>();
-
         st = new StringTokenizer(br.readLine());
+        
+        Stack<Node> stack = new Stack<>();
         stack.add(new Node(Integer.parseInt(st.nextToken()), 0));
+
         for(int i=1; i<N; i++) {
             int t = Integer.parseInt(st.nextToken());
             if(!stack.isEmpty()) {
@@ -30,29 +28,25 @@ public class Main {
             }
             stack.add(new Node(t, i));
         }
-
+        
         while(!stack.isEmpty()) {
             Node node = stack.pop();
             ret[node.idx] = -1;
         }
-
+        
         for(int n : ret) {
-            bw.write(n + " ");
+            sb.append(n).append(" ");
         }
 
-        bw.flush();
+        System.out.println(sb);
         br.close();
-        bw.close();
     }
 
     static class Node {
-        int value;
-        int idx;
-
+        int value; int idx;
         public Node(int value, int idx) {
             this.value = value;
             this.idx = idx;
         }
     }
-
 }
