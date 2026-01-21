6df9d835e24c3b4cfa6a7bfb1466aeb4891f63c6
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-01-21 19:23:14 +0900

    [Silver IV] Title: 문자열 집합, Time: 2296 ms, Memory: 865784 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,52 +1,53 @@
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int[] parent;
+    Map<String, Boolean> isUsed = new HashMap<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken()); //문자열의 개수
-        int M = Integer.parseInt(st.nextToken()); //검사할 문자열의 개수
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        Node root = new Node();
+        Node graph = new Node();
         for(int i=0; i<N; i++) {
-            st = new StringTokenizer(br.readLine());
-            String tmp = st.nextToken();
-            Node now = root;
-            for(int j=0; j<tmp.length(); j++) {
-                char t = tmp.charAt(j);
-                if(now.characters[t-'a'] == null) {
-                    now.characters[t-'a'] = new Node();
-                }
-                now = now.characters[t-'a'];
-                if(j==tmp.length()-1) {
-                    now.isLeaf = true;
-                }
+            String line = br.readLine();
+            Node curr = graph;
+            for(char c : line.toCharArray()) {
+                if(curr.child[c-'a'] == null) curr.child[c-'a'] = new Node();
+                curr = curr.child[c-'a'];
             }
-        }
-        int sum = 0;
-        for(int i=0; i<M; i++) {
-            st = new StringTokenizer(br.readLine());
-            String tmp = st.nextToken();
-            Node now = root;
-            for(int j=0; j<tmp.length(); j++) {
-                char t = tmp.charAt(j);
-                now = now.characters[t-'a'];
-                if(now == null) break;
-                if(j==tmp.length()-1 && now.isLeaf) sum++;
-            }
+            curr.isLeaf = true;
         }
 
-        sb.append(sum);
+        int cnt = 0;
+        for(int i=0; i<M; i++) {
+            String line = br.readLine();
+            boolean isExist = true;
+            Node curr = graph;
+            for(char c : line.toCharArray()) {
+                if(curr.child[c-'a'] == null) {
+                    isExist = false;
+                    break;
+                }
+                curr = curr.child[c-'a'];
+            }
+            if(!curr.isLeaf) isExist = false;
+            if(isExist) cnt++;
+        }
+
+        sb.append(cnt);
+
         System.out.println(sb);
         br.close();
-
     }
 
-    static class Node {
-        Node[] characters = new Node[26];
+    static class Node{
+        Node[] child = new Node[26];
         boolean isLeaf;
     }
 }
