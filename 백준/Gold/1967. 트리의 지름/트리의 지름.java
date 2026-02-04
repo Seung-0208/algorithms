96f19503608f601fa4b4f6223c25722b7bd5ab77
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-02-04 17:15:20 +0900

    [Gold IV] Title: 트리의 지름, Time: 168 ms, Memory: 22792 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -1,58 +1,61 @@
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int max = Integer.MIN_VALUE;
     static ArrayList<Node>[] graph;
     static boolean[] isVisited;
-    static long answer;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
-
-        int n = Integer.parseInt(st.nextToken()); //노드의 개수
-        isVisited = new boolean[n+1];
+        int n = Integer.parseInt(br.readLine());
         graph = new ArrayList[n+1];
-        for(int i=1; i<=n; i++) graph[i] = new ArrayList<>();
 
+        for(int i=1; i<=n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        isVisited = new boolean[n+1];
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        StringTokenizer st;
         for(int i=0; i<n-1; i++) {
             st = new StringTokenizer(br.readLine());
-            int parent = Integer.parseInt(st.nextToken());
-            int child = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-            graph[parent].add(new Node(child, weight));
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            graph[a].add(new Node(b, w));
         }
-        DFS(1);
 
-        sb.append(answer);
+        DFS(1);
+        sb.append(max);
         System.out.println(sb);
         br.close();
     }
+
+    static int DFS(int node) {
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        for(Node n : graph[node]) {
+            if(!isVisited[n.n]) {
+                int distance = DFS(n.n) + n.w;
+                if(max1 < distance) {
+                    max2 = max1;
+                    max1 = distance;
+                }
+                else if(distance > max2) max2 = distance;
+            }
+        }
+        max = Math.max(max, max1+max2);
+        return max1;
+    }
+
     static class Node {
-        int node;
-        int weight;
-        public Node(int node, int weight) {
-            this.node = node;
-            this.weight = weight;
+        int n, w;
+        public Node(int n, int w) {
+            this.n = n;
+            this.w = w;
         }
     }
 
-    static long DFS(int node) {
-        isVisited[node] = true;
-        long max1 = 0;
-        long max2 = 0;
-        for(Node child : graph[node]) {
-            if(!isVisited[child.node]) {
-                long childSum = DFS(child.node) + child.weight;
-                if(childSum > max1) {
-                    max2 = max1;
-                    max1 = childSum;
-                } else if (childSum > max2) {
-                    max2 = childSum;
-                }
-            }
-        }
-        answer = Math.max(answer, max1+max2);
-        return max1;
-    }
 }
