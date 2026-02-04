5b8184fe08910be9942933676fa2131ad000353b
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-02-04 17:41:03 +0900

    [Gold II] Title: 공항, Time: 192 ms, Memory: 23336 KB -BaekjoonHub

--- 백준/Gold/10775. 공항/공항.java
+++ 백준/Gold/10775. 공항/공항.java
@@ -2,48 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] parents;
+    static int[] parent;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int G = Integer.parseInt(st.nextToken()); //게이트 수
-        parents = new int[G+1];
-        for(int i=1; i<=G; i++) parents[i] = i;
-        st = new StringTokenizer(br.readLine());
-        int P = Integer.parseInt(st.nextToken()); //비행기 수
+        StringBuilder sb = new StringBuilder();
 
+        int G = Integer.parseInt(br.readLine());
+        parent = new int[G+1];
+        for(int i=1; i<=G; i++) parent[i] = i;
+        int P = Integer.parseInt(br.readLine());
         int cnt = 0;
-        boolean isBreak = false;
-        for(int i=1; i<=P; i++) {
-            st = new StringTokenizer(br.readLine());
-            int g = Integer.parseInt(st.nextToken());
-            int parentG = find(g);
-            parents[g] = parentG;
-            if(parentG != 0 && !isBreak) { //parentG에 도킹
-                cnt++;
-                union(parentG-1, parentG);
-            } else {
-                isBreak = true;
-            }
+        for(int i=0; i<P; i++) {
+            int g = Integer.parseInt(br.readLine());
+            parent[g] = find(g);
+            if(parent[g] == 0) break;
+            cnt++;
+            union(parent[g], --parent[g]);
         }
 
-        bw.write(cnt+"\n");
-        bw.flush();
-        bw.close();
+        sb.append(cnt);
+
+        System.out.println(sb);
         br.close();
     }
 
-    static int find(int n) {
-        if(parents[n] == n) return n;
-        return find(parents[n]);
+    static int find(int a) {
+        if(a == parent[a]) return a;
+        return find(parent[a]);
     }
-
     static void union(int a, int b) {
-        parents[a] = find(a);
-        parents[b] = find(b);
-        if(parents[a] != parents[b]) {
-            parents[parents[b]] = parents[a];
+//        parent[a] = find(a);
+        parent[b] = find(b);
+        if(parent[a] != parent[b]) {
+            parent[parent[a]] = parent[b];
         }
     }
 }
