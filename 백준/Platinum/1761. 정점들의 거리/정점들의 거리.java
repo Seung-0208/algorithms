b7ebc5ef377ec19810497b716494cc9adab6567f
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-02-18 16:19:22 +0900

    [Platinum V] Title: 정점들의 거리, Time: 392 ms, Memory: 45752 KB -BaekjoonHub

--- 백준/Platinum/1761. 정점들의 거리/정점들의 거리.java
+++ 백준/Platinum/1761. 정점들의 거리/정점들의 거리.java
@@ -1,35 +1,31 @@
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+    static int N, K;
     static ArrayList<Node>[] graph;
-    static boolean[] isVisited;
+    static int[][] parent, dist;
     static int[] depth;
-    static int[][] parent;
-    static int[][] upDist;
-    static int kMax;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken()); //수의 개수
+        N = Integer.parseInt(br.readLine());
+
+        int t =2;
+        K = 1;
+        while(t<=N) {
+            t = t*2;
+            K++;
+        }
+        parent = new int[N+1][K+1];
+        dist = new int[N+1][K+1];
+        depth = new int[N+1];
         graph = new ArrayList[N+1];
         for(int i=1; i<=N; i++) graph[i] = new ArrayList<>();
-        isVisited = new boolean[N+1];
-        depth = new int[N+1];
-
-        int temp = 1;
-        kMax = 0;
-        while(temp <= N) {
-            temp <<= 1;
-            kMax++;
-        }
-
-        parent = new int[kMax+1][N+1];
-        upDist = new int[kMax+1][N+1];
-
+        StringTokenizer st;
         for(int i=0; i<N-1; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
@@ -41,10 +37,10 @@ public class Main {
 
         BFS();
 
-        for(int k=1; k<=kMax; k++) {
+        for(int k=1; k<=K; k++) {
             for(int i=1; i<=N; i++) {
-                parent[k][i] = parent[k-1][parent[k-1][i]];
-                upDist[k][i] = upDist[k-1][i] + upDist[k-1][parent[k-1][i]];
+                parent[i][k] = parent[parent[i][k-1]][k-1];
+                dist[i][k] = dist[i][k-1] + dist[parent[i][k-1]][k-1];
             }
         }
 
@@ -53,73 +49,69 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            sb.append(getDist(a, b)).append("\n");
+            sb.append(getLCA(a, b)).append("\n");
         }
+
 
         System.out.println(sb);
-        br.close();
     }
 
-    static int getDist(int a, int b) {
-        int deeper, lighter;
-        int sum = 0;
-        if(depth[a] > depth[b]) {
-            deeper = a;
-            lighter = b;
-        } else {
-            deeper = b;
-            lighter = a;
+    static int getLCA(int a, int b) {
+        int ans = 0;
+        if(depth[a] < depth[b]) {
+            int temp = a;
+            a = b;
+            b = temp;
         }
 
-        for(int k=kMax; k>=0; k--) {
-            if(Math.pow(2, k) <= depth[deeper]-depth[lighter]) {
-                sum += upDist[k][deeper];
-                deeper = parent[k][deeper];
+        for(int k=K; k>=0; k--) {
+            if((int)Math.pow(2,k) <= depth[a]-depth[b]) {
+                ans += dist[a][k];
+                a = parent[a][k];
             }
         }
 
-        for(int k=kMax; k>=0; k--) {
-            if(parent[k][deeper] != parent[k][lighter]) {
-                sum += upDist[k][deeper];
-                deeper = parent[k][deeper];
-                sum += upDist[k][lighter];
-                lighter = parent[k][lighter];
+        for(int k=K; k>=0; k--) {
+            if(parent[a][k] != parent[b][k]) {
+                ans += dist[a][k];
+                ans += dist[b][k];
+                a = parent[a][k];
+                b = parent[b][k];
             }
         }
 
-        if(deeper != lighter) {
-            sum += upDist[0][deeper];
-            sum += upDist[0][lighter];
+        if(a != b) {
+            ans += dist[a][0];
+            ans += dist[b][0];
         }
-        return sum;
+
+        return ans;
     }
 
     static void BFS() {
         Queue<Integer> q = new ArrayDeque<>();
         q.add(1);
-        isVisited[1] = true;
-
-        int cnt = 0;
         int level = 1;
+        int cnt = 0;
         int childCnt = 1;
-        while(!q.isEmpty()) {
-            int cur = q.poll();
-            depth[cur] = level;
+        boolean[] isVisited = new boolean[N+1];
 
-            for(Node n : graph[cur]) {
-                if(!isVisited[n.node]) {
-                    isVisited[n.node] = true;
-                    q.add(n.node);
-                    parent[0][n.node] = cur;
-                    upDist[0][n.node] = n.weight;
+        while(!q.isEmpty()) {
+            int now = q.poll();
+            isVisited[now] = true;
+            depth[now] = level;
+            for(Node next : graph[now]) {
+                if(!isVisited[next.node]) {
+                    parent[next.node][0] = now;
+                    dist[next.node][0] = next.weight;
+                    q.add(next.node);
                 }
             }
 
             cnt++;
-
             if(cnt == childCnt) {
-                level++;
                 cnt = 0;
+                level++;
                 childCnt = q.size();
             }
         }
