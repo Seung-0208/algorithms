b6d6b44d72e922e1bd6d490820d832433539e7c9
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-02-22 22:22:05 +0900

    [Platinum V] Title: LCA 2, Time: 776 ms, Memory: 88496 KB -BaekjoonHub

--- 백준/Platinum/11438. LCA 2/LCA 2.java
+++ 백준/Platinum/11438. LCA 2/LCA 2.java
@@ -1,32 +1,31 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int kmax;
-    static ArrayList<Integer>[] graph;
     static int[][] parent;
+    static ArrayList<Integer>[] graph;
+    static int N, K;
     static int[] depth;
-    static boolean[] isVisited;
-    public static void main(String[] args) throws IOException{
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
 
+        N = Integer.parseInt(br.readLine());
+        K = 1;
+        int temp = 2;
+
+        while(temp <= N) {
+            temp *= 2;
+            K++;
+        }
+
+        parent = new int[N+1][K+1];
+        depth = new int[N+1];
         graph = new ArrayList[N+1];
         for(int i=1; i<=N; i++) graph[i] = new ArrayList<>();
-        isVisited = new boolean[N+1];
-        depth = new int[N+1];
-
-        //최대 k값(kmax) 구하기
-        int temp = 1;
-        kmax = 0;
-        while(temp<=N) {
-            temp <<= 1;
-            kmax++;
-        }
-        parent = new int[kmax+1][N+1];
-
+        StringTokenizer st;
         for(int i=0; i<N-1; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
@@ -37,58 +36,54 @@ public class Main {
 
         BFS();
 
-        //부모 정보 채우기
-        for(int i=1; i<=kmax; i++) {
+        for(int i=1; i<=K; i++) {
             for(int j=1; j<=N; j++) {
-                parent[i][j] = parent[i-1][parent[i-1][j]];
+                parent[j][i] = parent[parent[j][i-1]][i-1];
             }
         }
 
-        st = new StringTokenizer(br.readLine());
-        int M = Integer.parseInt(st.nextToken());//쌍의 개수
+        int M = Integer.parseInt(br.readLine());
 
         for(int i=0; i<M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            int LCA = getLCA(a, b);
-            bw.write(LCA+"\n");
+            sb.append(getLCA(a, b)).append("\n");
         }
-        bw.flush();
-        br.close();
-        bw.close();
+
+        System.out.println(sb);
     }
 
     static int getLCA(int a, int b) {
         if(depth[a] < depth[b]) {
             int temp = a;
-            a  = b;
+            a = b;
             b = temp;
         }
-        //a와 b의 높이 맞추기
-        for(int k = kmax; k>=0; k--) {
-            if((int)Math.pow(2,k) <= depth[a]-depth[b]) {
-                a = parent[k][a];
+
+        for(int k = K; k>=0; k--) {
+            if((int)(Math.pow(2, k)) <= depth[a]-depth[b]) {
+                a = parent[a][k];
             }
         }
 
-        //a와 b의 값 맞추기
-        for(int k=kmax; k>=0; k--) {
-            if(parent[k][a] != parent[k][b]) {
-                a = parent[k][a];
-                b = parent[k][b];
+        for(int k=K; k>=0; k--) {
+            if(parent[a][k] != parent[b][k]) {
+                a = parent[a][k];
+                b = parent[b][k];
             }
         }
-        int LCA = a;
+
         if(a!=b) {
-            LCA = parent[0][LCA];
+            a = parent[a][0];
         }
-
-        return LCA;
+        return a;
     }
 
     static void BFS() {
-        Queue<Integer> q = new LinkedList<>();
+        boolean[] isVisited = new boolean[N+1];
+        isVisited[1] = true;
+        Queue<Integer> q = new ArrayDeque<>();
         q.add(1);
 
         int level = 1;
@@ -96,20 +91,22 @@ public class Main {
         int childCnt = 1;
 
         while(!q.isEmpty()) {
-            int now = q.poll();
-            isVisited[now] = true;
-            depth[now] = level;
+            int cur = q.poll();
+            depth[cur] = level;
 
-            for(int next : graph[now]) {
+            for(int next : graph[cur]) {
                 if(!isVisited[next]) {
+                    parent[next][0] = cur;
                     q.add(next);
-                    parent[0][next] = now;
+                    isVisited[next] = true;
                 }
             }
+
             cnt++;
+
             if(cnt == childCnt) {
-                level++;
                 cnt = 0;
+                level++;
                 childCnt = q.size();
             }
         }
