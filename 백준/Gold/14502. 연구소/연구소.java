b5695a4771678b1b3113769b9310fbfe64ee3bb8
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-02-23 18:02:19 +0900

    [Gold IV] Title: 연구소, Time: 344 ms, Memory: 101104 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,83 +1,88 @@
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int N, M;
     static int[][] map;
-    static int[][] guide = {{0,0,-1,1}, {1,-1,0,0}};
-    static int ans;
-    static ArrayList<int[]> empties = new ArrayList<>();
     static ArrayList<int[]> viruses = new ArrayList<>();
+    static ArrayList<int[]> safes = new ArrayList<>();
+    static int max = Integer.MIN_VALUE;
+    static int N, M;
+    static int[][] guide = {{1,-1,0,0}, {0,0,1,-1}};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        N = Integer.parseInt(st.nextToken()); //행
-        M = Integer.parseInt(st.nextToken()); //열
         map = new int[N][M];
+
         for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if(map[i][j] == 0) empties.add(new int[]{i, j});
-                if(map[i][j] == 2) viruses.add(new int[]{i,j});
+                if(map[i][j] == 2) viruses.add(new int[] {i, j});
+                if(map[i][j] == 0) safes.add(new int[] {i, j});
             }
         }
 
-        tracking(0, 0);
 
-        sb.append(ans);
+        tracking(0, 0);
+        sb.append(max);
+
         System.out.println(sb);
-        br.close();
     }
 
-    static void tracking(int startIdx, int cnt) {
+    static void tracking(int idx, int cnt) {
         if(cnt == 3) {
-            ans = Math.max(ans, spread());
+            getSafeCount();
             return;
         }
 
-        for(int i=startIdx; i<empties.size(); i++) {
-            int[] pos = empties.get(i);
-            int r = pos[0], c = pos[1];
-
-            map[r][c] = 1;
-            tracking(i+1, cnt+1);
-            map[r][c] = 0;
+        for(int i=idx; i<safes.size(); i++) {
+            int nr = safes.get(i)[0];
+            int nc = safes.get(i)[1];
+            if(map[nr][nc] != 1) {
+                map[nr][nc] = 1;
+                tracking(i+1, cnt+1);
+                map[nr][nc] = 0;
+            }
         }
     }
 
-    static int spread() {
-        int[][] copy = new int[N][M];
+    static void getSafeCount() {
+        int[][] copyMap = new int[N][M];
         for(int i=0; i<N; i++) {
-            if(M >= 0) System.arraycopy(map[i], 0, copy[i], 0, M);
+            System.arraycopy(map[i], 0, copyMap[i], 0, M);
         }
 
-        ArrayDeque<int[]> q = new ArrayDeque<>();
-        for(int[] v : viruses) q.add(new int[]{v[0], v[1]});
+        Queue<int[]> q = new ArrayDeque<>();
+        boolean[][] isVisited = new boolean[N][M];
+        for(int[] v : viruses) {
+            q.add(v.clone());
+            isVisited[v[0]][v[1]] = true;
+        }
 
         while(!q.isEmpty()) {
-            int[] cur = q.poll();
-            int r = cur[0], c = cur[1];
-
+            int[] curr = q.poll();
             for(int i=0; i<4; i++) {
-                int nr = r + guide[0][i], nc = c + guide[1][i];
-                if(nr >= 0 && nr < N && nc >= 0 && nc < M && copy[nr][nc] == 0) {
-                    copy[nr][nc] = 2;
-                    q.add(new int[]{nr, nc});
+                int nr = curr[0] + guide[0][i];
+                int nc = curr[1] + guide[1][i];
+                if(nr >= 0 && nr < N && nc >= 0 && nc < M && !isVisited[nr][nc] && copyMap[nr][nc] == 0) {
+                    copyMap[nr][nc] = 2;
+                    q.add(new int[] {nr, nc});
                 }
             }
         }
 
-        int safe = 0;
+        int cnt = 0;
         for(int i=0; i<N; i++) {
             for(int j=0; j<M; j++) {
-                if(copy[i][j] == 0) safe++;
+                if(copyMap[i][j] == 0) cnt++;
             }
         }
-
-        return safe;
+        max = Math.max(cnt, max);
     }
 }
