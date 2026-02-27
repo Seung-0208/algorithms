b518ceb3232182a2c605fcac94a61191eb3eca24
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-02-27 21:51:57 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 15.00 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
@@ -1,26 +1,23 @@
 class Solution
 {
-    public int solution(int [][]dp)
+    public int solution(int [][]board)
     {
-        int N = dp.length;
-        int M = dp[0].length;
-        
-        int max = 0;
-        
-        for(int i=1; i<N; i++) {
-            for(int j=1; j<M; j++) {
-                if(dp[i][j] == 1) {
-                    int temp = Math.min(dp[i][j-1], dp[i-1][j]);
-                    dp[i][j] = Math.min(temp, dp[i-1][j-1])+1;
-                    max = Math.max(dp[i][j], max);
+        int n = board.length;
+        int m = board[0].length;
+        if(n==1 && m==1 && board[0][0]==1) return 1;
+        int answer = Integer.MIN_VALUE;
+        for(int i=1; i<n; i++) {
+            for(int j=1; j<m; j++) {
+                if(board[i][j] == 1) {
+                    int temp = Math.min(board[i-1][j], board[i][j-1]);
+                    temp = Math.min(temp, board[i-1][j-1]);
+                    board[i][j] = temp+1;
+                    answer = Math.max(board[i][j], answer);
                 }
             }
         }
-        
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-        if(N==1 && M==1 && dp[0][0]==1) return 1;
-        return max*max;
+
+        return answer*answer;
     }
 }
