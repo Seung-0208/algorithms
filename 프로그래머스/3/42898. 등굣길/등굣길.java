7de323393ef020cb13e90b45c6d761f7dd8f7f4b
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-03-04 18:06:07 +0900

    [level 3] Title: 등굣길, Time: 0.73 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,21 +1,23 @@
 class Solution {
-    public int solution(int m, int n, int[][] puddles) {
+    public int solution(int m, int n, int[][] puddles) { //m:열, n:행
         int[][] map = new int[n+1][m+1];
-        int[][] dp = new int[n+1][m+1];
+        map[1][1] = 1;
+        
         for(int[] p : puddles) {
-            map[p[1]][p[0]] = 1;
+            map[p[1]][p[0]] = -1;
         }
         
-        dp[1][1] =1;
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=m; j++) {
-                if(map[i][j] != 1) {
-                    if(i==1 && j==1) continue;
-                    dp[i][j] = (dp[i-1][j] + dp[i][j-1])%1000000007;
+                if(map[i][j] != -1 && !(i == 1 && j == 1)) {
+                    map[i][j] = (map[i-1][j] + map[i][j-1])%1000000007;
+                } else if(map[i][j]==-1) {
+                    map[i][j] = 0;
                 }
             }
         }
-        int answer = dp[n][m];
+        
+        int answer = map[n][m];
         return answer;
     }
 }
