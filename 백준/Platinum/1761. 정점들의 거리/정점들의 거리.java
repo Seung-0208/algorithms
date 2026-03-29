4e33f6fb898ea5d4d068392f28126472ad6f32f5
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-03-29 14:39:23 +0900

    [Platinum V] Title: 정점들의 거리, Time: 424 ms, Memory: 45592 KB -BaekjoonHub

--- 백준/Platinum/1761. 정점들의 거리/정점들의 거리.java
+++ 백준/Platinum/1761. 정점들의 거리/정점들의 거리.java
@@ -4,43 +4,43 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int N, K;
-    static ArrayList<Node>[] graph;
-    static int[][] parent, dist;
+    static int[][] parent;
+    static ArrayList<int[]>[] graph;
+    static int[][] distance;
     static int[] depth;
+    static int N, k;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         N = Integer.parseInt(br.readLine());
-
-        int t =2;
-        K = 1;
-        while(t<=N) {
-            t = t*2;
-            K++;
+        k = 1;
+        while(Math.pow(2,k)<=N){
+            k++;
         }
-        parent = new int[N+1][K+1];
-        dist = new int[N+1][K+1];
-        depth = new int[N+1];
+
+        parent = new int[k+1][N+1];
+        distance = new int[k+1][N+1];
         graph = new ArrayList[N+1];
         for(int i=1; i<=N; i++) graph[i] = new ArrayList<>();
+        depth = new int[N+1];
+
         StringTokenizer st;
         for(int i=0; i<N-1; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
-            graph[a].add(new Node(b, w));
-            graph[b].add(new Node(a, w));
+            graph[a].add(new int[] {b,w});
+            graph[b].add(new int[] {a,w});
         }
 
-        BFS();
+        initialize();
 
-        for(int k=1; k<=K; k++) {
-            for(int i=1; i<=N; i++) {
-                parent[i][k] = parent[parent[i][k-1]][k-1];
-                dist[i][k] = dist[i][k-1] + dist[parent[i][k-1]][k-1];
+        for(int i=1; i<=k; i++) {
+            for(int j=1; j<=N; j++) {
+                parent[i][j] = parent[i-1][parent[i-1][j]];
+                distance[i][j] = distance[i-1][parent[i-1][j]] + distance[i-1][j];
             }
         }
 
@@ -49,79 +49,74 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            sb.append(getLCA(a, b)).append("\n");
+            sb.append(getDistance(a,b)).append("\n");
         }
 
-
         System.out.println(sb);
+        br.close();
     }
 
-    static int getLCA(int a, int b) {
-        int ans = 0;
+    static int getDistance(int a, int b) {
         if(depth[a] < depth[b]) {
             int temp = a;
             a = b;
             b = temp;
         }
 
-        for(int k=K; k>=0; k--) {
-            if((int)Math.pow(2,k) <= depth[a]-depth[b]) {
-                ans += dist[a][k];
-                a = parent[a][k];
+        int ans = 0;
+        for(int i=k; i>=0; i--) {
+            if(Math.pow(2,i) <= depth[a] - depth[b]) {
+                ans += distance[i][a];
+                a = parent[i][a];
             }
         }
 
-        for(int k=K; k>=0; k--) {
-            if(parent[a][k] != parent[b][k]) {
-                ans += dist[a][k];
-                ans += dist[b][k];
-                a = parent[a][k];
-                b = parent[b][k];
+        for(int i=k; i>=0; i--) {
+            if(parent[i][a] != parent[i][b]) {
+                ans += distance[i][a];
+                ans += distance[i][b];
+                a = parent[i][a];
+                b = parent[i][b];
             }
         }
 
         if(a != b) {
-            ans += dist[a][0];
-            ans += dist[b][0];
+            ans += distance[0][a];
+            ans += distance[0][b];
         }
 
         return ans;
     }
 
-    static void BFS() {
+    static void initialize() {
         Queue<Integer> q = new ArrayDeque<>();
         q.add(1);
+        boolean[] isVisited = new boolean[N+1];
+        isVisited[1] = true;
+        int childCnt = 1;
         int level = 1;
         int cnt = 0;
-        int childCnt = 1;
-        boolean[] isVisited = new boolean[N+1];
-
         while(!q.isEmpty()) {
-            int now = q.poll();
-            isVisited[now] = true;
-            depth[now] = level;
-            for(Node next : graph[now]) {
-                if(!isVisited[next.node]) {
-                    parent[next.node][0] = now;
-                    dist[next.node][0] = next.weight;
-                    q.add(next.node);
+            int curr = q.poll();
+            depth[curr] = level;
+            for(int[] child : graph[curr]) {
+                int node = child[0];
+                int weight = child[1];
+                if(!isVisited[node]) {
+                    parent[0][node] = curr;
+                    distance[0][node] = weight;
+                    q.add(node);
+                    isVisited[node] = true;
                 }
             }
 
             cnt++;
+
             if(cnt == childCnt) {
                 cnt = 0;
-                level++;
                 childCnt = q.size();
+                level++;
             }
-        }
-    }
-
-    static class Node {
-        int node, weight;
-        public Node(int node, int weight) {
-            this.node = node;
-            this.weight = weight;
         }
     }
 }
