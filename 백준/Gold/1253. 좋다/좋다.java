fa47ec107614d2ce4c19739d198513ba791c94fe
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-03-31 12:06:21 +0900

    [Gold IV] Title: 좋다, Time: 156 ms, Memory: 14752 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,36 +1,54 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.IntStream;
 
-public class Main{
+public class Main {
+    static int ans = 0;
+    static long[] nums;
+    static int N;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        long[] nums = new long[N];
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++) nums[i] = Long.parseLong(st.nextToken());
-        Arrays.sort(nums); //0번
-        int cnt = 0;
-        for(int i=0; i<N; i++) { //1번
-            int s_pt = 0, e_pt = N-1;
-            long cur = nums[i];
-            while(s_pt<e_pt) { //2번
-                long temp = nums[s_pt] + nums[e_pt];
-                if(cur > temp) s_pt++;
-                else if(cur < temp) e_pt--;
-                else { //만약 둘이 같다면
-                    if(s_pt != i && e_pt != i) {
-                        cnt++;
-                        break;
-                    }
-                    else if(s_pt==i) s_pt++;
-                    if(e_pt==i) e_pt--;
-                }
+        nums = new long[N];
+        for(int i=0; i<N; i++) {
+            nums[i] = Long.parseLong(st.nextToken());
+        }
+
+        if(N > 2) {
+            Arrays.sort(nums);
+
+            for(int i=0; i<N; i++) {
+                calculate(nums[i], i);
             }
         }
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write(String.valueOf(cnt));
-        bw.flush();
+
+        sb.append(ans);
+        System.out.println(sb);
+        br.close();
+    }
+
+    static void calculate(long n, int idx) {
+        int i=0, j=N-1;
+        while(i<j) {
+            if(i==idx) {
+                i++;
+                continue;
+            }
+            if(j==idx) {
+                j--;
+                continue;
+            }
+            long temp = nums[i] + nums[j];
+            if(temp < n) {
+                i++;
+            } else if(temp > n) {
+                j--;
+            } else {
+                ans++;
+                break;
+            }
+        }
     }
 }
