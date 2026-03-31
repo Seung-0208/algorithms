580c0e8596d78a9bc4abd8a2b854ac73f5412eb7
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-03-31 11:22:57 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 808 ms, Memory: 102932 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,61 +1,64 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int[] arr;
-    static int[] copy;
-    static int N;
-    public static void main(String[] args) throws IOException {
+        static int N;
+        static int[] nums;
+        static int[] copy;
+        public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        N = Integer.parseInt(br.readLine()); //배열의 개수
-        arr = new int[N];
+        N = Integer.parseInt(br.readLine());
+        nums = new int[N];
         copy = new int[N];
 
-        for(int i=0; i<N; i++) arr[i] = Integer.parseInt(br.readLine());
-
-        mergeNSort(0, N-1);
-
-        for(int n : arr) {
-            sb.append(n).append("\n");
+        for(int i=0; i<N; i++ ) {
+            nums[i] = Integer.parseInt(br.readLine());
         }
 
+        mergeSort(0, N-1);
 
+        for(int t : nums) sb.append(t).append("\n");
         System.out.println(sb);
         br.close();
     }
 
-    static void mergeNSort(int start, int end) {
-        if(start >= end) return;
-        int m = (start+end)/2;
+    static void mergeSort(int s, int e){
+        if(s >= e) return;
 
-        mergeNSort(start, m);
-        mergeNSort(m+1, end);
+        int m = (s+e)/2;
+        mergeSort(s, m);
+        mergeSort(m+1, e);
 
-        System.arraycopy(arr, start, copy, start, end-start+1);
+        System.arraycopy(nums, s, copy, s, (e-s)+1);
 
-        int i = start, j = m+1, now = start;
-        while(i <= m && j <= end) {
+        int i=s, j=m+1;
+        int k=s;
+
+        while(i<=m && j<=e) {
             if(copy[i] < copy[j]) {
-                arr[now] = copy[i];
+                nums[k] = copy[i];
                 i++;
-            }
-            else {
-                arr[now] = copy[j];
+            } else {
+                nums[k] = copy[j];
                 j++;
             }
-            now++;
+            k++;
         }
 
-        while(i < m+1) {
-            arr[now] = copy[i];
-            i++; now++;
+        while(i<=m) {
+            nums[k] = copy[i];
+            i++;
+            k++;
         }
 
-        while(j < end+1) {
-            arr[now] = copy[j];
-            j++; now++;
+        while(j<=e) {
+            nums[k] = copy[j];
+            j++;
+            k++;
         }
     }
 }
