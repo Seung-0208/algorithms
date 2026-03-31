e300592b740846a4cd7868dbe0c175967d2441c3
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-03-31 11:03:47 +0900

    [Silver IV] Title: ATM, Time: 496 ms, Memory: 19360 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,27 +1,45 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+        public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-        int[] nums = new int[N];
+        LinkedList<Integer> people = new LinkedList<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
+            people.add(Integer.parseInt(st.nextToken()));
         }
 
-        Arrays.sort(nums);
-        int total = 0;
         for(int i=1; i<N; i++) {
-            nums[i] = nums[i] + nums[i-1];
-            total += nums[i];
+            int temp = people.get(i);
+            int j;
+            for(j=0; j<i; j++) {
+                if(people.get(j) > people.get(i)) {
+                    break;
+                }
+            }
+            people.remove(i);
+            people.add(j, temp);
         }
-        total+=nums[0];
 
-        sb.append(total).append("\n");
+
+
+        int[] sum = new int[N];
+        sum[0] = people.get(0);
+        for(int i=1; i<N; i++) {
+            sum[i] = sum[i-1] + people.get(i);
+        }
+        int ans = 0;
+        for(int i=0; i<N; i++) {
+            ans += sum[i];
+        }
+
+        sb.append(ans);
         System.out.println(sb);
         br.close();
     }
