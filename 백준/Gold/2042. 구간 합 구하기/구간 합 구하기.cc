457b58f3c346a9d27453c4a06f891b6f803b6897
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-04-08 12:43:43 +0900

    [Gold I] Title: 구간 합 구하기, Time: 164 ms, Memory: 18408 KB -BaekjoonHub

---
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <climits>
+
+using namespace std;
+
+vector<long long> tree;
+
+//트리 기준 인덱스 값
+long long getSum(int i, int j) {
+    long long sum = 0;
+    while(i<=j) {
+        if(i%2 != 0) {
+            sum += tree[i];
+        }
+        if(j%2 == 0) {
+            sum += tree[j];
+        }
+        i = (i+1)/2;
+        j = (j-1)/2;
+    }
+    return sum;
+}
+
+//트리 기준 인덱스 값
+void change(int idx, long long value) {
+    tree[idx] = value;
+    
+    while(idx/2 > 0) {
+        idx /= 2;
+        tree[idx] = tree[idx*2] + tree[idx*2+1];
+    }
+}
+
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, M, K;
+    cin >> N >> M >> K;
+
+    int t = 1;
+    int temp = 2;
+    while(temp <= N) temp *= 2;
+
+    tree.assign(temp*2, 0);
+
+    for(int i=0; i<N; i++) {
+        cin >> tree[temp+i];
+    }
+
+    //트리 초기화
+    for(int i=temp-1; i>0; i--) {
+        tree[i] = tree[i*2]+tree[i*2+1];
+    }
+
+    for(int i=0; i<M+K; i++) {
+        int a, b;
+        long long c;
+        cin >> a >> b >> c;
+
+        if(a == 1) change(b+temp-1, c);
+        if(a == 2) cout << getSum(b+temp-1, ((int)c)+temp-1) << "\n";
+    }
+
+
+    return 0;
+}
