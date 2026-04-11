be73922d6816ee7a2562e863fc37c32eb3cff332
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-04-11 16:59:38 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -18,37 +18,33 @@ int main() {
     vector<int> tails;
     tails.reserve(N);
 
-    vector<int> pos(N);
+    vector<int> pos(N, 0);
 
     for(int i=0; i<N; i++) {
         auto it = lower_bound(tails.begin(), tails.end(), nums[i]);
-        int idx = (int)(it - tails.begin());
+
+        int idx = it - tails.begin();
         if(it == tails.end()) {
             tails.push_back(nums[i]);
         } else {
             *it = nums[i];
         }
-
         pos[i] = idx;
+    }
+
+    int need = tails.size()-1;
+    vector<int> ans(tails.size(), 0);
+    for(int i=N-1; i>=0; i--) {
+        if(pos[i]==need) {
+            ans[need] = nums[i];
+            need--;
+        }
     }
 
     cout << tails.size() << "\n";
 
-    vector<int> ans(tails.size());
-    int need = tails.size()-1;
-
-    for(int i=N-1; i>=0; i--) {
-        if(pos[i] == need) {
-            ans[need]= nums[i];
-            need--;
-            if(need<0) break;
-        }
-    }
-
     for(int n : ans) {
         cout << n << " ";
     }
-
-
     return 0;
 }
