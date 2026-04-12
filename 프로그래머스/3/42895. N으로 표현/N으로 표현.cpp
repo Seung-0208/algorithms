a6f2dd74f811b239696b621280bfaaa0f17e9ae3
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-04-12 12:15:44 +0900

    [level 3] Title: N으로 표현, Time: 1847.06 ms, Memory: 1.06 GB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
@@ -4,50 +4,47 @@
 
 using namespace std;
 
-int makeRepeatNumber(int n, int N) {
+int makeRepeatNumber(int N, int cnt) {
     string temp = "";
-    for(int i=0; i<n; i++) temp += to_string(N);
+    for(int i=0; i<cnt; i++) {
+        temp += to_string(N);
+    }
     return stoi(temp);
 }
 
 int solution(int N, int number) {
     
-    vector<unordered_set<int>> cases(9);
+    vector<vector<long long>> cases(9, vector<long long>());
     
     for(int i=1; i<9; i++) {
-        int temp = makeRepeatNumber(i, N);
-        cases[i].insert(temp);
-        if(temp == number) {
-            return i;
-        }
+        int t = makeRepeatNumber(N, i);
+        if(t == number) return i;
+        cases[i].push_back(t);
     }
     
     for(int i=2; i<9; i++) {
-        int s = 1;
-        int e = i-1;
-        while(s <= e && s+e == i) {
-            for(int a : cases[e]) {
-                for(int b : cases[s]) {
+        for(int j=1; j<=i-1; j++) {
+            for(long long a : cases[j]) {
+                for(long long b : cases[i-j]) {
                     if(a+b == number) return i;
-                    cases[i].insert(a+b);
+                    cases[i].push_back(a+b);
                     if(a-b == number) return i;
-                    cases[i].insert(a-b);
+                    cases[i].push_back(a-b);
                     if(b-a == number) return i;
-                    cases[i].insert(b-a);
+                    cases[i].push_back(b-a);
                     if(a*b == number) return i;
-                    cases[i].insert(a*b);
+                    cases[i].push_back(a*b);
                     if(b != 0) {
                         if(a/b == number) return i;
-                        cases[i].insert(a/b);
+                        cases[i].push_back(a/b);
                     }
                     if(a != 0) {
                         if(b/a == number) return i;
-                        cases[i].insert(b/a);
+                        cases[i].push_back(b/a);
                     }
                 }
             }
-            s++; e--;
-        }
+        }    
     }
     
     return -1;
