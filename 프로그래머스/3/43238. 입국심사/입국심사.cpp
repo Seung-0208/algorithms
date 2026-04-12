2e17de2bc1c719f7372f10bb3ce4559b1b73f590
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-04-12 11:51:39 +0900

    [level 3] Title: 입국심사, Time: 32.82 ms, Memory: 6.75 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -4,33 +4,28 @@
 
 using namespace std;
 
-bool isPossible(long long T, vector<int>& times, int n) {
-    long long sum = 0;
-    for(int t : times) {
-        sum += (T/t);
-        if(sum >= n) return true;
-    }
-    return false;
-}
-
 long long solution(int n, vector<int> times) {
     
-    sort(times.begin(), times.end());
-    
-    long long s = 0;
-    long long e = 1LL* times[0]*n;
-    
+    long long s = 0, e = 10000000000000;
+        
     long long answer = 0;
     
-    
     while(s<=e) {
-        long long T = (s+e)/2;
-        bool result = isPossible(T, times, n);
-        if(result) {
-            answer = T;
-            e = T-1;
+        long long k = (s+e)/2;
+        cout << k << "\n";
+        
+        long long cnt = 0;
+        
+        for(int t : times) {
+            cnt += k/t;
+            if(cnt >= n) break;
+        }
+        
+        if(cnt >= n) {
+            answer = k;
+            e = k-1;
         } else {
-            s = T+1;
+            s = k+1;
         }
     }
     
