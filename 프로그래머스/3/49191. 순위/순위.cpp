d1284ac42782ceb08a0cae14955bfc7f7e79efb5
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-04-12 11:08:47 +0900

    [level 3] Title: 순위, Time: 1.42 ms, Memory: 4.04 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.cpp
+++ 프로그래머스/3/49191. 순위/순위.cpp
@@ -1,34 +1,28 @@
 #include <string>
 #include <vector>
-#include <bits/stdc++.h>
 
 using namespace std;
 
 int solution(int n, vector<vector<int>> results) {
-    
-    cout << true << "\n";
-    cout << false << "\n";
-    
     vector<vector<bool>> isWin(n+1, vector<bool>(n+1, false));
     vector<int> winCnt(n+1, 0);
     vector<int> loseCnt(n+1, 0);
     
-    
     for(vector<int> r : results) {
         int win = r[0];
         int lose = r[1];
+        isWin[win][lose] = true;
         winCnt[win]++;
         loseCnt[lose]++;
-        isWin[win][lose] = true;
     }
     
     for(int k=1; k<=n; k++) {
-        for(int s=1; s<=n; s++) {
-            for(int e=1; e<=n; e++) {
-                if(isWin[s][e] == false && isWin[s][k] && isWin[k][e]) {
-                    isWin[s][e] = true;
-                    winCnt[s]++;
-                    loseCnt[e]++;
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n; j++) {
+                if(isWin[i][k] && isWin[k][j] && !isWin[i][j]) {
+                    isWin[i][j] = true;
+                    winCnt[i]++;
+                    loseCnt[j]++;
                 }
             }
         }
