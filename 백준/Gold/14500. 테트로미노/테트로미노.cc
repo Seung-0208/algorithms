7bffbeb15415f49bcea9771c793fcea9e2e8524c
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-04-25 16:37:02 +0900

    [Gold IV] Title: 테트로미노, Time: 72 ms, Memory: 2964 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.cc
+++ 백준/Gold/14500. 테트로미노/테트로미노.cc
@@ -4,33 +4,34 @@
 
 using namespace std;
 
-int N, M;
 vector<vector<int>> board;
+vector<vector<int>> guide = {{1,-1,0,0}, {0,0,1,-1}};
 vector<vector<bool>> isVisited;
-int guide[2][4] = {{1,-1,0,0}, {0,0,1,-1}};
-int ans = INT_MIN;
 
-void DFS(int r, int c, int depth, int sum) {
-    if(depth == 4) {
+int ans = INT_MIN;
+int N, M;
+
+void backtracking(int cnt, int sum, int r, int c) {
+    if(cnt == 4) {
         if(ans < sum) ans = sum;
         return;
     }
 
     for(int i=0; i<4; i++) {
-        int nr = r+guide[0][i];
-        int nc = c+guide[1][i];
+        int nr = r + guide[0][i];
+        int nc = c + guide[1][i];
 
         if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
         if(isVisited[nr][nc]) continue;
 
         isVisited[nr][nc] = true;
-        DFS(nr, nc, depth+1, sum+board[nr][nc]);
+        backtracking(cnt+1, sum+board[nr][nc], nr, nc);
         isVisited[nr][nc] = false;
     }
 }
 
-void calculateMiddle(int r, int c) {
-    int minValue = INT_MAX;
+void middleTracking(int r, int c) {
+    int smallest = INT_MAX;
     int sum = board[r][c];
     int cnt = 0;
 
@@ -38,23 +39,21 @@ void calculateMiddle(int r, int c) {
         int nr = r+guide[0][i];
         int nc = c+guide[1][i];
 
-        if(nr<0 || nr>=N || nc<0 || nc>=M) continue;
-        if(minValue > board[nr][nc]) minValue = board[nr][nc];
-        sum += board[nr][nc];
+        if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
         cnt++;
+        if(smallest > board[nr][nc]) smallest = board[nr][nc];
+        sum += board[nr][nc];
     }
 
-    if(cnt == 3 && ans < sum) {
-        ans = sum;
-        return;
+    if(cnt == 3) {
+        if(ans < sum) ans = sum;
     }
     if(cnt == 4) {
-        sum -= minValue;
+        sum -= smallest;
         if(ans < sum) ans = sum;
-        return;
     }
-    return;
 }
+
 
 int main() {
 
@@ -62,6 +61,7 @@ int main() {
     cin.tie(nullptr);
 
     cin >> N >> M;
+
     board.assign(N, vector<int>(M, 0));
     isVisited.assign(N, vector<bool>(M, false));
 
@@ -70,19 +70,17 @@ int main() {
             cin >> board[i][j];
         }
     }
-    
+
     for(int i=0; i<N; i++) {
         for(int j=0; j<M; j++) {
             isVisited[i][j] = true;
-            DFS(i, j, 1, board[i][j]); //시작점
+            backtracking(1, board[i][j], i, j);
             isVisited[i][j] = false;
-            
-            calculateMiddle(i, j);
+            middleTracking(i, j);
         }
     }
 
     cout << ans << endl;
-    
 
     return 0;
 }
