e532e98ad4ff8a52c7d860cba3c6f268bae2aae7
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-04-25 16:18:38 +0900

    [Gold III] Title: 경사로, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.cc
+++ 백준/Gold/14890. 경사로/경사로.cc
@@ -1,36 +1,32 @@
 #include <iostream>
 #include <bits/stdc++.h>
+#include <cmath>
 
 using namespace std;
 
-bool calculate(vector<int> line, int N, int L) {
-    vector<bool> isUsed(N, false);
+bool isRoad(vector<int>& row, int length) {
+    vector<bool> isUsed(row.size(), false);
 
-    for(int i=0; i<N-1; i++) {
-        int cur = line[i];
-        int next = line[i+1];
-
-        if(cur==next) continue;
-        if(cur-next >= 2 || cur-next <= -2) return false;
-
-        if(cur < next) {
-            if(i+1-L < 0) return false;
-            for(int j=i; j>=i+1-L; j--) {
-                if(line[j] != cur) return false;
+    for(int i=1; i<row.size(); i++) {
+        
+        if(abs(row[i]-row[i-1]) > 1) return false;
+        if(row[i-1] == row[i]-1) {
+            if(i-length < 0) return false;
+            for(int j=i-length; j<=i-1; j++) {
+                if(row[j] != row[i-1]) return false;
                 if(isUsed[j]) return false;
-            }
-            for(int j=i; j>=i+1-L; j--) {
                 isUsed[j] = true;
             }
         }
+    }
 
-        if(cur > next) {
-            if(i+L >= N) return false;
-            for(int j=i+1; j<=i+L; j++) {
-                if(line[j] != next) return false;
+    for(int i=row.size()-2; i>=0; i--) {
+        if(abs(row[i]-row[i+1]) > 1) return false;
+        if(row[i+1] == row[i]-1) {
+            if(i+length >= row.size()) return false;
+            for(int j=i+1; j<=i+length; j++) {
+                if(row[j] != row[i+1]) return false;
                 if(isUsed[j]) return false;
-            }
-            for(int j=i+1; j<=i+L; j++) {
                 isUsed[j] = true;
             }
         }
@@ -47,33 +43,32 @@ int main() {
     int N, L;
     cin >> N >> L;
 
-    vector<vector<int>> board(N, vector<int>(N, 0));
+    vector<vector<int>> map(N, vector<int>(N, 0));
 
     for(int i=0; i<N; i++) {
         for(int j=0; j<N; j++) {
-            cin >> board[i][j];
+            cin >> map[i][j];
         }
     }
 
-    int cnt = 0;
+    int ans = 0;
+    for(int i=0; i<N; i++) {
+        vector<int> temp(N, 0);
+        for(int j=0; j<N; j++) {
+            temp[j] = map[i][j];
+        }
+        if(isRoad(temp, L)) ans++;
+    }
 
     for(int i=0; i<N; i++) {
         vector<int> temp(N, 0);
         for(int j=0; j<N; j++) {
-            temp[j] = board[i][j];
+            temp[j] = map[j][i];
         }
-        if(calculate(temp, N, L)) cnt++;
+        if(isRoad(temp, L)) ans++;
     }
 
-    for(int i=0; i<N; i++) {
-        vector<int> temp(N, 0);
-        for(int j=0; j<N; j++) {
-            temp[j] = board[j][i];
-        }
-        if(calculate(temp, N, L)) cnt++;
-    }
-
-    cout << cnt << endl;
+    cout << ans << endl;
 
     return 0;
 }
