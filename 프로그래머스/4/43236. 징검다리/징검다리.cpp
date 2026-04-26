f88d2db024add1b9dfcf1997e72d058fc4afed44
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-04-26 15:02:10 +0900

    [level 4] Title: 징검다리, Time: 4.03 ms, Memory: 5.23 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.cpp
+++ 프로그래머스/4/43236. 징검다리/징검다리.cpp
@@ -5,41 +5,33 @@
 using namespace std;
 
 int solution(int distance, vector<int> rocks, int n) {
-    vector<int> arr(rocks.size()+2, 0);
-    
     sort(rocks.begin(), rocks.end());
-    
-    arr[rocks.size()+1] = distance;
-    for(int i=1; i<=rocks.size(); i++) {
-        arr[i] = rocks[i-1];
+    vector<int> board(rocks.size()+2, 0);
+    for(int i=0; i<rocks.size(); i++) {
+        board[i+1] = rocks[i];
     }
+    board[rocks.size()+1] = distance;
     
-    int s = 0, e = distance*2;
+    int s = 0;
+    int e = distance*2;
     int answer = 0;
-    
     while(s<=e) {
-        int prev = 0; //마지막으로 남긴 돌
-        int removed = 0; //없앤 돌의 개수
-        
-        int k = (s+e)/2;
-        
-        bool isPossible = true;
+        int minDist = (s+e)/2;
+        int removed = 0;
+        int prev = 0;
         
         for(int i=1; i<rocks.size()+2; i++) {
-            if(arr[i]-prev < k) removed++;
-            else prev = arr[i];
-            
-            if(removed > n) {
-                isPossible = false;
-                break;
+            if(board[i]-prev < minDist) removed++;
+            else {
+                prev = board[i];
             }
+            if(removed > n) break;
         }
         
-        if(isPossible) {
-            s = k+1;
-            answer = k;
-        } else {
-            e = k-1;
+        if(removed > n) e = minDist - 1;
+        else {
+            answer = minDist;
+            s = minDist + 1;
         }
     }
     
