f70c0e463f504abcab1afcf477b570924ac6d0e8
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-04-26 14:48:52 +0900

    [level 3] Title: 순위, Time: 1.28 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.cpp
+++ 프로그래머스/3/49191. 순위/순위.cpp
@@ -1,28 +1,26 @@
 #include <string>
 #include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int solution(int n, vector<vector<int>> results) {
-    vector<vector<bool>> isWin(n+1, vector<bool>(n+1, false));
-    vector<int> winCnt(n+1, 0);
-    vector<int> loseCnt(n+1, 0);
-    
+    vector<vector<int>> winsInfo(n+1, vector<int>(n+1, 0));
+    vector<int> wins(n+1, 0);
+    vector<int> loses(n+1, 0);
     for(vector<int> r : results) {
-        int win = r[0];
-        int lose = r[1];
-        isWin[win][lose] = true;
-        winCnt[win]++;
-        loseCnt[lose]++;
+        winsInfo[r[0]][r[1]] = 1;
+        wins[r[0]]++;
+        loses[r[1]]++;
     }
     
     for(int k=1; k<=n; k++) {
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=n; j++) {
-                if(isWin[i][k] && isWin[k][j] && !isWin[i][j]) {
-                    isWin[i][j] = true;
-                    winCnt[i]++;
-                    loseCnt[j]++;
+                if(winsInfo[i][k]==1 && winsInfo[k][j]==1 && winsInfo[i][j] == 0) {
+                    winsInfo[i][j] = 1;
+                    wins[i]++;
+                    loses[j]++;
                 }
             }
         }
@@ -31,7 +29,7 @@ int solution(int n, vector<vector<int>> results) {
     int answer = 0;
     
     for(int i=1; i<=n; i++) {
-        if(winCnt[i] + loseCnt[i] == n-1) answer++;
+        if(wins[i]+loses[i] == n-1) answer++;
     }
     
     return answer;
