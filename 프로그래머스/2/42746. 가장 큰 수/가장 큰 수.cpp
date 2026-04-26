11a0f366ed568126fe319360617763aa982c1d0d
LEE SEUNGYOUNG <122588528+Seung-0208@users.noreply.github.com>
2026-04-26 15:06:57 +0900

    [level 2] Title: 가장 큰 수, Time: 567.21 ms, Memory: 7.44 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -5,32 +5,28 @@
 using namespace std;
 
 struct CMP {
-    bool operator()(int a, int b) const {
-        string as = to_string(a);
-        string bs = to_string(b);
+    bool operator() (int a, int b) const {
+        int v1 = stoi(to_string(a) + to_string(b));
+        int v2 = stoi(to_string(b) + to_string(a));
         
-        return (as+bs) < (bs+as);
+        return v1 < v2;
     }
 };
 
 string solution(vector<int> numbers) {
-    
     priority_queue<int, vector<int>, CMP> pq;
-    
     bool isAllZero = true;
     for(int n : numbers) {
-        if(n!=0 && isAllZero) isAllZero = false;
         pq.push(n);
+        if(n!=0) isAllZero = false;
     }
     
     if(isAllZero) return "0";
     
     string answer = "";
-    
     while(!pq.empty()) {
         answer += to_string(pq.top());
         pq.pop();
     }
-        
     return answer;
 }
